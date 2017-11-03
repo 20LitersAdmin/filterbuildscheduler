24ded9ed6bfa6d1e1e5fbda85fd06a99cad9e496
Ross-Hunter <ross-hunter@ross-hunter.com>
2017-11-03 08:55:44 -0400

    RSPEC baby

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module BuildPlanner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
