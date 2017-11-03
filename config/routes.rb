bb7a5de92195b4e35ebb9ac868a7dbe0235bc1e8
Ross-Hunter <ross-hunter@ross-hunter.com>
2017-11-03 09:53:09 -0400

    Throw in a redirect to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('/admin')
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
