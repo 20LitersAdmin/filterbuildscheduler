ba1e6733a33bd7630b05f5c567835ae96971c930
Sam DeHaan <dehaansa@gmail.com>
2017-11-03 21:29:21 -0400

    Fix indentation

--- db/migrate/20171104002831_add_more_user_fields.rb
+++ db/migrate/20171104002831_add_more_user_fields.rb
@@ -1,7 +1,7 @@
 class AddMoreUserFields < ActiveRecord::Migration[5.1]
   def change
-  	add_column :users, :is_archived, :boolean, default: false
-  	add_column :users, :signed_consent_form_on, :date
+    add_column :users, :is_archived, :boolean, default: false
+    add_column :users, :signed_consent_form_on, :date
     add_column :users, :qualified_technology_id, :integer, array: true, default: []
     add_column :users, :primary_location_id, :integer
   end
