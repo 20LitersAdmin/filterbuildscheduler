8955784fd58e4d7c59791b9025a7f4d5764fa600
Sam DeHaan <dehaansa@gmail.com>
2017-11-03 22:35:21 -0400

    Fix routes for registrations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
-  resources :registrations
+  resources :events do
+    resources :registrations
+  end
 end
