09ebc41722a712cfe5f3c683f57d861e769b68d4
Roger Roelofs <roger@fusionary.com>
2017-11-04 15:56:54 -0400

    fix user profile edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,26 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find params[:id]
+    @user.update user_params
+    redirect_to events_path
+  end
 
   def delete
     @user = authorize User.find(params[:id])
     @user.delete
     redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :fname,
+                                  :lname,
+                                  :email,
+                                  :password,
+                                  :password_confirmation
   end
 
 end
