9e261f7dd904a9a84069121243f85aeb06506afb
Ross-Hunter <ross-hunter@ross-hunter.com>
2017-11-04 16:43:39 -0400

    Make event sign up better

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < ApplicationController
   before_action :find_registration, only: [:edit, :update, :destroy]
-  
+
   def create
     waiver_accepted = params[:registration].delete(:waiver_accepted)
     raise ActionController::BadRequest, "must accept waiver to participate" if waiver_accepted == '0'
@@ -24,10 +24,10 @@ class RegistrationsController < ApplicationController
 
     redirect_to events_path
   end
-  
+
   def edit
   end
-  
+
   def update
     authorize @registration
     @registration.update(registration_params)
@@ -39,7 +39,7 @@ class RegistrationsController < ApplicationController
     @registration.delete
     redirect_to event_path(@registration.event)
   end
-  
+
   private
 
   def user_params
@@ -47,17 +47,13 @@ class RegistrationsController < ApplicationController
   end
 
   def registration_params
-    if params[:registration][:leader] == '1' && !User.find(params[:registration][:user_id]).is_leader?
-      raise ActionController::BadRequest, "Cannot register as leader if you are not a leader"
-    end
-
     params.require(:registration).permit(:event_id,
                                          :user_id,
                                          :leader,
                                          :guests_registered,
                                          :accomodations)
   end
-  
+
   def find_registration
     @registration = Registration.find(params[:id])
   end
