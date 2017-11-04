31af1de3d3951f76d9c72050e8a1383df9e5fb14
Chip Kragt <chiperific@gmail.com>
2017-11-04 13:46:03 -0400

    add user/id/show and user/id/edit ; link to my account on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   
   devise_for :users
 
-  get 'users/:id/show', to: 'users#show'
+  get 'users/:id/show', to: 'users#show', as: 'show_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch 'users/:id', to: 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
     resources :registrations
