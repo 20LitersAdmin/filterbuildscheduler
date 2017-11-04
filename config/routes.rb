92d1ed903cc80b4011c46d777855695e140e92b1
Sam DeHaan <dehaansa@gmail.com>
2017-11-04 12:10:43 -0400

    Add waiver to the registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'events#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
+
   devise_for :users
+
+  get :waiver, controller: :application
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
     resources :registrations
