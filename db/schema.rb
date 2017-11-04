92227349db0e1ac2918a1f7eb329bdef4fc6c1b4
Sam DeHaan <dehaansa@gmail.com>
2017-11-03 21:27:05 -0400

    Add foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104002831) do
+ActiveRecord::Schema.define(version: 20171104012406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,4 +92,7 @@ ActiveRecord::Schema.define(version: 20171104002831) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "locations"
+  add_foreign_key "registrations", "events"
+  add_foreign_key "registrations", "users"
 end
