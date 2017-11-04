28c68e7d35f39dd0a9f8419e4bb0dec179bc8699
Roger Roelofs <roger@fusionary.com>
2017-11-03 22:49:54 -0400

    more pundit biolerplate to show how to use it

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,10 @@
 class RegistrationsController < ApplicationController
+
+
+  def delete
+    @reg = authorize Registration.find(params[:id])
+    @reg.delete
+    redirect_to registrations_path
+  end
+
 end
--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -1,2 +1,9 @@
 class TechnologiesController < ApplicationController
+
+  def delete
+    @tech = authorize Technology.find(params[:id])
+    @tech.delete
+    redirect_to technologies_path
+  end
+
 end
