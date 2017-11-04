077b41018d03d352e5268145be5e1150d798bc56
Roger Roelofs <roger@fusionary.com>
2017-11-04 00:09:47 -0400

    common sense validators

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   belongs_to :location
   has_many :registrations
   has_many :users, through: :registrations
+
+
+  validates :start_time, :end_time, :title, presence: true
+  validates :min_registrations, :max_registrations, :min_leaders, :max_leaders, numericality: { only_integer: true, greater_than: 0 }
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+
+  validates :name, :address1, :city, :state, :zip, presence: true
 end
