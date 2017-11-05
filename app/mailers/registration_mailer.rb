830b99a084ec63ac11fdb53a3c82088142d5554a
James Gifford <james@armyofminions.com>
2017-11-04 22:26:20 -0400

    Add letter opener web and fix created email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -6,21 +6,22 @@ class RegistrationMailer < ApplicationMailer
     @registration = registration
     @recipient = registration.user
     @event = registration.event
+    @location = @event.location
     if @recipient.password.blank?
       @token = Devise.token_generator.generate(User, :reset_password_token)
-      @recipient.reset_password_token = hashed_token
+      @recipient.reset_password_token = @token
       @recipient.reset_password_sent_at = Time.now.utc
       @recipient.save
     end
 
-    mail(subject: "[20 Liters] Registration for #{@event.name} @ #{@event.start_time.to_formatted_s(:short)}")
+    mail(subject: "[20 Liters] Registration for #{@event.title} @ #{@event.start_time.to_formatted_s(:short)}")
   end
 
   def reminder(registration)
     @registration = registration
     @recipient = registration.user
     @event = registration.event
-    mail(subject: "[20 Liters] Reminder for #{@event.name} @ #{@event.start_time.to_formatted_s(:short)}")
+    mail(subject: "[20 Liters] Reminder for #{@event.title} @ #{@event.start_time.to_formatted_s(:short)}")
   end
 
 end
