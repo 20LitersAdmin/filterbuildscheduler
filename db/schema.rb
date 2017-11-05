35bde86c45bdf611c9f91fb6d85d22aa404341ce
Dan Drust <dan.drust@protrainings.com>
2017-11-04 20:54:36 -0400

    Implement attendance form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104230522) do
+ActiveRecord::Schema.define(version: 20171105004653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,11 @@ ActiveRecord::Schema.define(version: 20171104230522) do
     t.integer "location_id", null: false
     t.integer "technology_id"
     t.boolean "is_private", default: false, null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_events_on_deleted_at"
     t.integer "item_goal"
     t.integer "item_results"
+    t.datetime "deleted_at"
+    t.integer "attendance"
+    t.index ["deleted_at"], name: "index_events_on_deleted_at"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -121,7 +122,9 @@ ActiveRecord::Schema.define(version: 20171104230522) do
     t.integer "primary_location_id"
     t.string "fname"
     t.string "lname"
+    t.string "authentication_token", limit: 30
     t.datetime "deleted_at"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
