e8cca4c9bdf37fb9950a5066bd84cb51f5d5f26a
James Gifford <james@armyofminions.com>
2017-11-04 20:30:48 -0400

    Merged them in the wrong order, I guess?

--- db/schema.rb
+++ db/schema.rb
@@ -45,10 +45,10 @@ ActiveRecord::Schema.define(version: 20171104230522) do
     t.integer "location_id", null: false
     t.integer "technology_id"
     t.boolean "is_private", default: false, null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_events_on_deleted_at"
     t.integer "item_goal"
     t.integer "item_results"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_events_on_deleted_at"
   end
 
   create_table "locations", force: :cascade do |t|
