58e1b565481c428f2b2fb5e73afec811f3edc268
James Gifford <james@armyofminions.com>
2017-11-04 21:10:03 -0400

    Allow signups again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def waiver
     render partial: 'users/user_waiver_form'
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
   end
 end
