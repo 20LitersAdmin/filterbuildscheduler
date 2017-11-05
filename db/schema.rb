57a76e3ec4005f9970db139efd56e638a64c6713
James Gifford <james@armyofminions.com>
2017-11-04 20:59:46 -0400

    authenticaton token merge thing? hmm. :thinking_face:

--- db/schema.rb
+++ db/schema.rb
@@ -122,9 +122,7 @@ ActiveRecord::Schema.define(version: 20171105004653) do
     t.integer "primary_location_id"
     t.string "fname"
     t.string "lname"
-    t.string "authentication_token", limit: 30
     t.datetime "deleted_at"
-    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
