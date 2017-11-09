0184e2f7ac6ccb9c8b5270f0464ff8cfb79320cb
chiperific <chiperific@gmail.com>
2017-11-08 21:44:19 -0500

    rescue bad urls

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,4 +7,9 @@ class LocationsController < ApplicationController
     redirect_to locations_path
   end
 
+  def route_error
+    flash[:danger] = "That's not a real place."
+    redirect_to root_path
+  end
+
 end
