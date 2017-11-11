611d5c37ae8946022e01514981d5bea9063fc497
Ross-Hunter <ross-hunter@ross-hunter.com>
2017-11-11 12:28:10 -0500

    Authorize with policies. Rescue from permission errors

--- app/policies/registration_policy.rb
+++ app/policies/registration_policy.rb
@@ -1,21 +1,18 @@
 class RegistrationPolicy < ApplicationPolicy
-  attr_reader :user, :registration
-
-  def initialize(user, registration)
-    @user = user
-    @registration = registration
+  def create?
+    record.user == user
   end
 
   def destroy?
-    user.is_admin? || registration.user == user
+    user.is_admin? || record.user == user
   end
 
   def update?
     return true if user.is_admin?
-    if registration.leader
+    if record.leader
       user.is_leader
     else
-      registration.user == user
+      record.user == user
     end
   end
 end
