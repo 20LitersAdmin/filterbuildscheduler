5047206cada5c5fc39fc7d80a4be16f624758cc1
chiperific <chiperific@gmail.com>
2017-11-12 22:43:01 -0500

    add intellegence class

---
+++ app/services/intelligence.rb
@@ -0,0 +1,30 @@
+class Intellegence
+
+  def extrapolate(component)
+    # calculate the number of parts in a component
+    # adjust the count accordingly
+
+  def inventory_on_hand(part)
+    # don't count component::parts marked as completed_tech || completed_tech_boxes
+  end
+
+  def inventory_not_shipped(model)
+    # count component::parts marked as completed_tech, but not completed_tech_boxes
+  end
+
+  def predicition(model)
+    # how many do we have on hand?
+    # Look at last inventory.latest.date and subtract the blow-out of events.item_results
+  end
+
+  def forecast(model)
+    # when will this item need to be ordered? Based on inventory_on_hand
+    # look at upcoming builds::registrations && builds.length && builds.goal for associated technology#unit_rate vs predicted quantity
+  end
+
+  def suggest_goal(event)
+    # What should the event's goal be?
+    # look at past Event.where(technology_id: event.technology_id) and average item_results
+  end
+
+end
