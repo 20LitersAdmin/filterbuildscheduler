b3613faa3f6c2741ea281f2325567ad4577abc8c
chiperific <chiperific@gmail.com>
2017-11-15 19:45:48 -0500

    fix formatting on datetime pickers

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,9 +2,7 @@ $(document).on("turbolinks:load", function(){
   $('.datetimepicker').each(function(){
     var theValue = Date(this.value);
     $(this).datetimepicker({
-      date: this.value,
-      format: 'MMM DD YYYY HH:mm A'
+      date: this.value
     });
-    
   });
 });
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,18 @@
-module EventsHelper  
+module EventsHelper
   def time_for_form(event_time)
     if event_time.present?
       event_time.to_time.iso8601
     else
-      Time.now.iso8601
+      Time.now.strftime("%l:%M %P")
     end
-    
+
+  end
+
+  def date_for_form(event_date)
+    if event_date.present?
+      event_time.start_time.to_date
+    else
+      Date.today.strftime("%m/%d/%Y")
+    end
   end
 end
