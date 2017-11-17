d9b3a30f1d59e6bbf99781755b59998c02489287
Chip Kragt <chiperific@gmail.com>
2017-11-17 15:57:13 -0500

    seed the prod db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,197 +1,195 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Location.create!([
-  { name: 'Center of the Universe',
-    address1: "3501 Fairlanes Ave SW",
-    address2: "West side of the building",
-    city: "Grandville",
-    state: "MI",
-    zip: "49507",
-    map_url: "https://goo.gl/maps/ZgfJaae3ReJ2",
-    photo_url: "photo_url-filler-data",
-    instructions: "Park on West side of building, watch out for the alligator"
-  },
-  { name: 'Middle of Nowhere',
-    address1: "220 Alderman St",
-    address2: "West side of the building",
-    city: "Belding",
-    state: "MI",
-    zip: "48809",
-    map_url: "https://goo.gl/maps/ncrKcdMGsyK2",
-    photo_url: "photo_url-filler-data",
-    instructions: "Drive until civilization ends, then park and wait for nightfall."
-  }
-])
+# Location.create!([
+#   { name: 'Center of the Universe',
+#     address1: "3501 Fairlanes Ave SW",
+#     address2: "West side of the building",
+#     city: "Grandville",
+#     state: "MI",
+#     zip: "49507",
+#     map_url: "https://goo.gl/maps/ZgfJaae3ReJ2",
+#     photo_url: "photo_url-filler-data",
+#     instructions: "Park on West side of building, watch out for the alligator"
+#   },
+#   { name: 'Middle of Nowhere',
+#     address1: "220 Alderman St",
+#     address2: "West side of the building",
+#     city: "Belding",
+#     state: "MI",
+#     zip: "48809",
+#     map_url: "https://goo.gl/maps/ncrKcdMGsyK2",
+#     photo_url: "photo_url-filler-data",
+#     instructions: "Drive until civilization ends, then park and wait for nightfall."
+#   }
+# ])
 
-Technology.create!([
-  { name: "Bucket Filter",
-    description: "Designed by Village Water Filters and assembled by 20 Liters volunteers, this family-friendly build is open to people aged 4-104.",
-    ideal_build_length: 3,
-    ideal_group_size: 12,
-    ideal_leaders: 2,
-    family_friendly: true,
-    unit_rate: 2.13
-  },
-  { name: "Facility Filter",
-    description: "Got the engineering bug? Like to cut and glue PVC in complicated ways? Then this is the event for you.",
-    ideal_build_length: 4,
-    ideal_group_size: 4,
-    ideal_leaders: 1,
-    family_friendly: false,
-    unit_rate: 0.125
-  },
-])
+# Technology.create!([
+#   { name: "Bucket Filter",
+#     description: "Designed by Village Water Filters and assembled by 20 Liters volunteers, this family-friendly build is open to people aged 4-104.",
+#     ideal_build_length: 3,
+#     ideal_group_size: 12,
+#     ideal_leaders: 2,
+#     family_friendly: true,
+#     unit_rate: 2.13
+#   },
+#   { name: "Facility Filter",
+#     description: "Got the engineering bug? Like to cut and glue PVC in complicated ways? Then this is the event for you.",
+#     ideal_build_length: 4,
+#     ideal_group_size: 4,
+#     ideal_leaders: 1,
+#     family_friendly: false,
+#     unit_rate: 0.125
+#   },
+# ])
 
-Event.create!([
-  { start_time: 2.days.from_now,
-    end_time: 2.days.from_now,
-    title: "2 days from now",
-    description: "Open, public build. Anyone can come",
-    min_registrations: 1,
-    max_registrations: 30,
-    min_leaders: 1,
-    max_leaders: 3,
-    location: Location.first,
-    item_goal: 20,
-    technology_id: Technology.first.id
-  },
-  { start_time: 6.days.from_now,
-    end_time: 6.days.from_now,
-    title: "6 days from now",
-    description: "This one should be private, but the model is missing some fields.",
-    min_registrations: 1,
-    max_registrations: 10,
-    min_leaders: 1,
-    max_leaders: 3,
-    location: Location.second,
-    item_goal: 20,
-    technology_id: Technology.first.id
-  },
-  { start_time: 10.days.ago,
-    end_time: 9.days.ago,
-    title: "10 days ago",
-    description: "I'm an event in the past.",
-    min_registrations: 1,
-    max_registrations: 10,
-    min_leaders: 1,
-    max_leaders: 3,
-    location: Location.first,
-    item_goal: 20,
-    technology_id: Technology.second.id
-  }
-])
+# Event.create!([
+#   { start_time: 2.days.from_now,
+#     end_time: 2.days.from_now,
+#     title: "2 days from now",
+#     description: "Open, public build. Anyone can come",
+#     min_registrations: 1,
+#     max_registrations: 30,
+#     min_leaders: 1,
+#     max_leaders: 3,
+#     location: Location.first,
+#     item_goal: 20,
+#     technology_id: Technology.first.id
+#   },
+#   { start_time: 6.days.from_now,
+#     end_time: 6.days.from_now,
+#     title: "6 days from now",
+#     description: "This one should be private, but the model is missing some fields.",
+#     min_registrations: 1,
+#     max_registrations: 10,
+#     min_leaders: 1,
+#     max_leaders: 3,
+#     location: Location.second,
+#     item_goal: 20,
+#     technology_id: Technology.first.id
+#   },
+#   { start_time: 10.days.ago,
+#     end_time: 9.days.ago,
+#     title: "10 days ago",
+#     description: "I'm an event in the past.",
+#     min_registrations: 1,
+#     max_registrations: 10,
+#     min_leaders: 1,
+#     max_leaders: 3,
+#     location: Location.first,
+#     item_goal: 20,
+#     technology_id: Technology.second.id
+#   }
+# ])
 
-User.create!([
-  { email: "admin@email.com",
-    password: "password", password_confirmation: "password",
-    fname: "Admin",
-    lname: "Boss",
-    is_leader: true,
-    is_admin: true
-  },
-  { email: "leader1@email.com",
-    password: "password", password_confirmation: "password",
-    fname: "Leader",
-    lname: "One",
-    is_leader: true
-  },
-  { email: "leader2@email.com",
-    password: "password", password_confirmation: "password",
-    fname: "Leader",
-    lname: "Two",
-    is_leader: true
-  },
-  { email: "builder1@email.com",
-    password: "password", password_confirmation: "password",
-    fname: "Builder",
-    lname: "One"
-  },
-  { email: "builder2@email.com",
-    password: "password", password_confirmation: "password",
-    fname: "Builder",
-    lname: "Two"
-  },
-  { email: "builderMissing@email.com",
-    password: "password", password_confirmation: "password",
-    fname: "Builder",
-    lname: "Archived",
-    deleted_at: '2017-11-03 00:00:00'
-  },
-  { email: "builderNoPW@email.com",
-    fname: "Builder",
-    lname: "No Password"
-  },
-])
-
-Registration.create!([
-  {
-    user_id: 1,
-    guests_registered: 2,
-    event_id: 1,
-    leader: true
-  },
-  {
-    user_id: 2,
-    guests_registered: 4,
-    event_id: 1
-  },
-  {
-    user_id: 3,
-    guests_registered: 0,
-    event_id: 1
-  },
-  {
-    user_id: 4,
-    guests_registered: 6,
-    event_id: 1
-  },
-  {
-    user_id: 5,
-    guests_registered: 3,
-    event_id: 1
-  },
-  {
-    user_id: 7,
-    guests_registered: 5,
-    event_id: 1
-  },
-])
-
-Material.create!([
-  {
-    name: 'PVC 2-inch schd 40',
-    supplier: 'Lowes',
-    price_cents: 499,
-    min_order: 1,
-    order_id: "PLB213-45",
-    weeks_to_deliver: 1.5
-  },
-  {
-    name: 'PVC 1/2-inch schd 40',
-    supplier: 'Lowes',
-    price_cents: 299,
-    min_order: 1,
-    order_id: "PLB213-40",
-    weeks_to_deliver: 1.2
-  },
-  {
-    name: 'Hose 1/2-inch-ID clear flexible',
-    supplier: 'Hoses Online',
-    price_cents: 1600,
-    min_order: 1,
-    order_id: "HO-2345",
-    weeks_to_deliver: 3
-  },
-  {
-    name: 'Lumber 2x4x8 pine',
-    supplier: 'Lowes',
-    price_cents: 505,
-    min_order: 1,
-    order_id: "LMB456-01",
-    weeks_to_deliver: 1.5
-  }
-])
+# User.create!([
+#   { email: "admin@email.com",
+#     password: "password", password_confirmation: "password",
+#     fname: "Admin",
+#     lname: "Boss",
+#     is_leader: true,
+#     is_admin: true
+#   },
+#   { email: "leader1@email.com",
+#     password: "password", password_confirmation: "password",
+#     fname: "Leader",
+#     lname: "One",
+#     is_leader: true
+#   },
+#   { email: "leader2@email.com",
+#     password: "password", password_confirmation: "password",
+#     fname: "Leader",
+#     lname: "Two",
+#     is_leader: true
+#   },
+#   { email: "builder1@email.com",
+#     password: "password", password_confirmation: "password",
+#     fname: "Builder",
+#     lname: "One"
+#   },
+#   { email: "builder2@email.com",
+#     password: "password", password_confirmation: "password",
+#     fname: "Builder",
+#     lname: "Two"
+#   },
+#   { email: "builderMissing@email.com",
+#     password: "password", password_confirmation: "password",
+#     fname: "Builder",
+#     lname: "Archived",
+#     deleted_at: '2017-11-03 00:00:00'
+#   },
+#   { email: "builderNoPW@email.com",
+#     fname: "Builder",
+#     lname: "No Password"
+#   },
+# ])
+# Registration.create!([
+#   {
+#     user_id: 1,
+#     guests_registered: 2,
+#     event_id: 1,
+#     leader: true
+#   },
+#   {
+#     user_id: 2,
+#     guests_registered: 4,
+#     event_id: 1
+#   },
+#   {
+#     user_id: 3,
+#     guests_registered: 0,
+#     event_id: 1
+#   },
+#   {
+#     user_id: 4,
+#     guests_registered: 6,
+#     event_id: 1
+#   },
+#   {
+#     user_id: 5,
+#     guests_registered: 3,
+#     event_id: 1
+#   },
+#   {
+#     user_id: 7,
+#     guests_registered: 5,
+#     event_id: 1
+#   },
+# ])
+# Material.create!([
+#   {
+#     name: 'PVC 2-inch schd 40',
+#     supplier: 'Lowes',
+#     price_cents: 499,
+#     min_order: 1,
+#     order_id: "PLB213-45",
+#     weeks_to_deliver: 1.5
+#   },
+#   {
+#     name: 'PVC 1/2-inch schd 40',
+#     supplier: 'Lowes',
+#     price_cents: 299,
+#     min_order: 1,
+#     order_id: "PLB213-40",
+#     weeks_to_deliver: 1.2
+#   },
+#   {
+#     name: 'Hose 1/2-inch-ID clear flexible',
+#     supplier: 'Hoses Online',
+#     price_cents: 1600,
+#     min_order: 1,
+#     order_id: "HO-2345",
+#     weeks_to_deliver: 3
+#   },
+#   {
+#     name: 'Lumber 2x4x8 pine',
+#     supplier: 'Lowes',
+#     price_cents: 505,
+#     min_order: 1,
+#     order_id: "LMB456-01",
+#     weeks_to_deliver: 1.5
+#   }
+# ])
 
 # Part.create!([
 #   {
@@ -275,3 +273,23 @@ Material.create!([
 #     sample_weight: 1.2
 #   }
 # ])
+User.create!([
+  { email:  'andrew@20liters.org' , fname:  'Andrew'  , lname:  'Vantimmeren' , phone:  '616.710.6392' },
+  { email:  'lizzylouwho@gmail.com' , fname:  'Liz' , lname:  'Sitte' , phone:  '6164027949'  , is_leader: true },
+  { email:  'bjlong5@hotmail.com' , fname:  'Bob' , lname:  'Long'  , phone:  '6169016615'  , is_leader:  true },
+  { email:  'cmjohnson177@gmail.com'  , fname:  'Chris' , lname:  'Johnson' , phone:  '616-490-0356' },
+  { email:  'codye@foreway.com' , fname:  'Cody'  , lname:  'Ensing'  , phone:  '616-401-9862'      , does_inventory: true },
+  { email:  'dlathrop@steelcase.com'  , fname:  'Dave'  , lname:  'Lathrop' , phone:  '616-901-2912'  , is_leader:  true  , does_inventory: true },
+  { email:  'dougv@cqlcorp.com' , fname:  'Doug'  , lname:  'VandenHoek'  , phone:  '616.307.0018'  , is_leader:  true },
+  { email:  'jrcardinal@highpointelectric.us' , fname:  'Jon' , lname:  'Cardinal'  , phone:  '231.638.6089'  , is_leader:  true },
+  { email:  'lmont71@comcast.net' , fname:  'Linda' , lname:  'Montgomery'  , phone:  '6164194256'  , is_leader:  true  , does_inventory: true },
+  { email:  'liz.jasperse@gmail.com'  , fname:  'Liz' , lname:  'Jaspers' , phone:  '616-990-4402'      , does_inventory: true },
+  { email:  'peter.vandentoorn@gmail.com' , fname:  'Peter' , lname:  'VandenToorn' , phone:  '616.822.7498'      , is_admin:   true },
+  { email:  'snauta74@hotmail.com'  , fname:  'Steve' , lname:  'Nauta' , phone:  '6164467674'  , is_leader:  true  , does_inventory: true },
+  { email:  'thadcummings@gmail.com'  , fname:  'Thad'  , lname:  'Cummings'  , phone:  '248-982-7024'  , is_leader:  true },
+  { email:  'ta3jwiersma@gmail.com' , fname:  'Tim' , lname:  'Wiersma' , phone:  '616-914-7971'      , does_inventory: true },
+  { email:  'thamel53@gmail.com'  , fname:  'Tom' , lname:  'Hamel' , phone:  '231.301.2957'  , is_leader:  true },
+  { email:  'tommaas@hotmail.com' , fname:  'Tom' , lname:  'Maas'  , phone:  '616.901.6113'  , is_leader:  true },
+  { email:  'tonytheclimber@gmail.com'  , fname:  'Tony'  , lname:  'Kelly' , phone:  '6163899936'  , is_leader:  true  , does_inventory: true },
+  { email:  'bullers_us@yahoo.com'  , fname:  'Vern'  , lname:  'Bullers' , phone:  '541-221-8579'  , is_leader:  true }
+])
