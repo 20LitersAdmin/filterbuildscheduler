9c3721d19f5fabd4303adc59592786a545f09b93
chiperific <chiperific@gmail.com>
2017-11-18 11:27:07 -0500

    fix you_are_leader on event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -145,7 +145,7 @@ class Event < ApplicationRecord
     end
   end
   def you_are_leader(user)
-    if user.is_leader && registrations.where(user_id: user.id).where(leader: true).present?
+    if user&.is_leader && registrations.where(user_id: user.id).where(leader: true).present?
       "(including you)"
     end
   end
