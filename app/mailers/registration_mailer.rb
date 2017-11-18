21749b79e9d87fddf4b59b81171f62d38d7ee59b
chiperific <chiperific@gmail.com>
2017-11-18 15:38:50 -0500

    add gcal link and iCal attachment to builder registration emails

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -8,6 +8,14 @@ class RegistrationMailer < ApplicationMailer
     @recipient = registration.user
     @event = registration.event
     @location = @event.location
+    @summary = "[20 Liters] Filter Build: " + @event.technology.name
+    @attachment_title = "20Liters_filterbuild_" + @event.start_time.strftime("%Y%m%dT%H%M") + ".ical"
+
+    if @event.leaders_registered.count == 1
+      @leader_count_text = "Your leader is:"
+    elsif @event.leaders_registered.count > 1
+      @leader_count_text = "Your leaders are:"
+    end
 
     if @recipient.encrypted_password.blank?
       @token = Devise.token_generator.generate(User, :reset_password_token)
@@ -17,11 +25,21 @@ class RegistrationMailer < ApplicationMailer
       @recipient.save
     end
 
-    if @event.leaders_registered.count == 1
-      @leader_count_text = "Your leader is:"
-    elsif @event.leaders_registered.count > 1
-      @leader_count_text = "Your leaders are:"
+    @description = render partial: 'details.text'
+    @details = @description.gsub("\n","%0A").gsub(" ","%20")
+
+    cal = Icalendar::Calendar.new
+    cal.event do |e|
+      e.dtstart = @event.start_time
+      e.dtend = @event.end_time
+      e.organizer = "mailto:filterbuilds@20liters.org"
+      e.attendee = @recipient
+      e.location = @location.addr_one_liner
+      e.summary = @summary
+      e.description = @description
     end
+    cal.append_custom_property('METHOD', 'REQUEST')
+    mail.attachments[@attachment_title] = { mime_type: 'text/calendar', content: cal.to_ical }
 
     mail(to: @recipient.email, subject: "[20 Liters] You registered for a filter build on #{@event.mailer_time}")
   end
