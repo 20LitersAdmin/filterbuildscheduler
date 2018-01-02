24b6b6110c42ea3cf9be96449193b0850f5fe4ab
Chip Kragt <chiperific@gmail.com>
2018-01-02 16:02:44 -0500

    kindful scaffold with passing test

---
+++ spec/services/kindful_client_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe KindfulClient do
+  let(:user1) { build :user }
+  let(:client) { KindfulClient.new }
+
+  describe "update_user" do
+    fit 'takes user data and sends it to kindful' do
+      http_spy = spy
+      body_args = {
+        id: 1,
+        fname: "Person",
+        lname: "Last",
+        email: "email@test.com",
+        phone: "555-555-5555"
+      }
+
+      arguments = {
+        headers: client.headers,
+        body: client.body(**body_args)
+      }
+      expect(KindfulClient).to receive(:post).with('/imports', arguments).and_return(http_spy)
+      user2 = User.new(body_args)
+      user2.save
+    end
+  end
+end
