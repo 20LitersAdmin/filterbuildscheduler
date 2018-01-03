135d1a32d3cb1ce5f4bca3bb938fc2034c4fd232
Chip Kragt <chiperific@gmail.com>
2018-01-03 15:47:16 -0500

    check all policies and routes

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -17,7 +17,7 @@ module ErrorHandler
   end
 
   def record_not_found
-    flash[:warning] = "Nothing was found."
+    flash[:danger] = "Nothing was found."
     redirect_to root_path
   end
 end
--- app/policies/registration_policy.rb
+++ app/policies/registration_policy.rb
@@ -1,43 +1,36 @@
 class RegistrationPolicy < ApplicationPolicy
+  attr_reader :user, :registration
+
+  def initialize(user, registration)
+    @user = user
+    @registration = registration
+  end
+
   def create?
-    #user.admin_or_leader? || record.user == user
     true
   end
 
   def destroy?
-    user.admin_or_leader? || record.user == user
+    user&.admin_or_leader? || registration.user == user
   end
 
   def update?
-    return true if user.is_admin?
-    if record.leader
-      user.is_leader
-    else
-      record.user == user
-    end
+    user&.admin_or_leader? || registration.user == user
   end
 
   def index?
-    user.admin_or_leader?
+    user&.admin_or_leader?
   end
 
   def new?
-    user.admin_or_leader?
+    user&.admin_or_leader?
   end
 
   def edit?
-    user.admin_or_leader?
+    user&.admin_or_leader? || registration.user == user
   end
 
   def show?
-    user.admin_or_leader?
-  end
-
-  def messenger?
-    user.admin_or_leader?
-  end
-
-  def sender?
-    user.admin_or_leader?
+    user&.admin_or_leader?
   end
 end
