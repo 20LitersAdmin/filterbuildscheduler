57ab9d70015497791729866fb83df273f479e8e7
Chip Kragt <chiperific@gmail.com>
2018-01-03 13:34:14 -0500

    task is running twice

--- app/jobs/registration_reminder_job.rb
+++ app/jobs/registration_reminder_job.rb
@@ -3,9 +3,9 @@ class RegistrationReminderJob < ApplicationJob
 
   def perform(*args)
     Event.where('start_time > ? and start_time < ?', Time.zone.now + 1.days, Time.zone.now + 2.days).each do |e|
-      EventMailer.delay.reminder(e)
+      EventMailer.remind_admins(e).deliver_later
       e.registrations.each do |r|
-        RegistrationMailer.delay.reminder(r)
+        RegistrationMailer.reminder(r).deliver_later
       end
     end
   end
