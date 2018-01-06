7df1018d62d5732a33063c73b53d3230dc571375
Chip Kragt <chiperific@gmail.com>
2018-01-05 23:39:07 -0500

    User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,68 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user1) { create :user }
 
+  describe "must be valid" do
+    let(:good_user) {build :user }
+    let(:blank_password) { build :user, password: ""}
+    let(:no_fname) { build :user, fname: nil }
+    let(:no_lname) { build :user, lname: nil }
+    let(:no_email) { build :user, email: nil }
+    
+    it "only saves when valid" do
+      expect(good_user.save).to eq true
+      expect(blank_password.save).to eq true 
+    end
+
+    it "doesn't save if not valid" do
+      expect(no_fname.save).to eq false
+      expect(no_lname.save).to eq false
+      expect(no_email.save).to eq false
+    end
+
+  end
+
+  describe "#ensure_authentication_token" do
+    subject { user1.authentication_token }
+    it { should_not be nil }
+  end
+
+  describe "#update_kindful" do
+    pending 'takes user data and sends it to kindful_client' do
+      @kindful_spy = spy
+      expect(KindfulClient).to receive(:new).and_return(@kindful_spy)
+      user1
+      expect(@kindful_spy).to have_received(:update_kindful)
+    end
+  end
+
+  describe "#name" do
+    it 'concatentates fname and lname' do
+      expect(user1.name).to eq("#{user1.fname} #{user1.lname}")
+    end
+  end
+
+  describe "#can_lead_event?" do
+    let(:event) { create :event }
+    let(:qualified) { create :leader }
+    let(:unqualified) { create :user }
+
+    it "can lead an event when the join table has a record" do
+      qualified.technologies << event.technology
+      expect(qualified.technologies.exists?(event.technology.id)).to be_truthy
+      expect(qualified.can_lead_event?(event)).to be_truthy
+      expect(unqualified.technologies.exists?(event.technology.id)).to be_falsey
+      expect(unqualified.can_lead_event?(event)).to be_falsey
+    end
+
+    it "can't lead an event if they aren't a leader" do
+      qualified.technologies << event.technology
+      expect(qualified.is_leader).to be_truthy
+      expect(qualified.can_lead_event?(event)).to be_truthy
+      expect(unqualified.is_leader).to be_falsey
+      expect(unqualified.can_lead_event?(event)).to be_falsey
+    end
+  end
+
   describe 'available_events' do
     let(:event1) { create :event }
     let(:event2) { create :event }
@@ -30,15 +92,6 @@ RSpec.describe User, type: :model do
       expect(user1.available_events).to include(event1)
       expect(user1.available_events).to include(event2)
       expect(user1.available_events).to include(private_event)
-    end
-  end
-
-  describe "update_kindful" do
-    pending 'takes user data and sends it to kindful_client' do
-      @kindful_spy = spy
-      expect(KindfulClient).to receive(:new).and_return(@kindful_spy)
-      user1
-      expect(@kindful_spy).to have_received(:update_kindful)
     end
   end
 end
