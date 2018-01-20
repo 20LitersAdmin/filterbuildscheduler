0228c46e9eba28ce37aac2a65bb4d7e202fa3ed2
Chip Kragt <chiperific@gmail.com>
2018-01-20 10:00:50 -0500

    fix migration issue

--- db/migrate/20180120145432_add_default_to_event_attendance.rb
+++ db/migrate/20180120145432_add_default_to_event_attendance.rb
@@ -1,6 +1,6 @@
 class AddDefaultToEventAttendance < ActiveRecord::Migration[5.1]
   def change
 
-    change_column_default :events, :attendance, default: 0
+    change_column_default :events, :attendance, 0
   end
 end
