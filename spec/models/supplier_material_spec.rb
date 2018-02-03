af06a63b4cd70825b9e02489e8ac6900e96b85a0
Chip Kragt <chiperific@gmail.com>
2018-02-03 15:57:31 -0500

    tests for supplier model

--- spec/models/supplier_material_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SupplierMaterial, type: :model do
-  
-end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
-  
+  let(:supplier) { create :supplier }
+
+  describe "must be valid" do
+    let(:no_name) { build :supplier, name: nil }
+    let(:bad_email) { build :supplier, email: "not an email AT gmail DOT com" }
+    let(:bad_POC_email) { build :supplier, POC_email: "heythere.gmail.com" }
+    let(:no_url_scheme) { build :supplier, url: "wwww.noscheme.com" }
+    let(:no_url_host) { build :supplier, url: "https://" }
+    let(:bad_url) { build :supplier, url: "http://www.creeds-blog.blogspot.net.info.biz" }
+
+
+    fit "in order to save" do
+      expect(supplier.save).to eq true
+      expect { no_name.save!(validate: false) }.to raise_error ActiveRecord::NotNullViolation
+      expect(bad_email.save).to be_falsey
+      expect(bad_POC_email.save).to be_falsey
+      expect(no_url_scheme.save).to be_falsey
+      expect(no_url_host.save).to be_falsey
+      expect(bad_url.save).to be_falsey
+    end
+  end
 end
