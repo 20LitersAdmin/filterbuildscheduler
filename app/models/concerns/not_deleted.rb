7ee8cc511e811cf046d43b7af8b80c38af1277f3
Chip Kragt <chiperific@gmail.com>
2018-02-03 19:56:59 -0500

    create global scope for rails_admin

---
+++ app/models/concerns/not_deleted.rb
@@ -0,0 +1,51 @@
+module NotDeleted
+  extend ActiveSupport::Concern
+
+  included do
+    scope :active, -> { where(deleted_at: nil ) }
+  end
+end
+
+class Component < ApplicationRecord
+  include NotDeleted
+end
+
+class Count < ApplicationRecord
+  include NotDeleted
+end
+
+class Event < ApplicationRecord
+  include NotDeleted
+end
+
+class Inventory < ApplicationRecord
+  include NotDeleted
+end
+
+class Location < ApplicationRecord
+  include NotDeleted
+end
+
+class Material < ApplicationRecord
+  include NotDeleted
+end
+
+class Part < ApplicationRecord
+  include NotDeleted
+end
+
+class Registration < ApplicationRecord
+  include NotDeleted
+end
+
+class Supplier < ApplicationRecord
+  include NotDeleted
+end
+
+class Technology < ApplicationRecord
+  include NotDeleted
+end
+
+class User < ApplicationRecord
+  include NotDeleted
+end
