62ea32ccf1db8a550bd95c90a1b2c2e0645bd2dd
Chip Kragt <chiperific@gmail.com>
2018-02-08 23:43:50 -0500

    tests green

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,11 +3,10 @@ class WebhooksController < ApplicationController
 
   def receive
     head :ok
-    
+
     @json = params[:data][:object].as_json
     @json.deep_symbolize_keys!
-
-    binding.pry
+    
     # first check for the application
     if @json[:application] == "ca_14yEk8gp5dbBbYDnndXU9yTNM3Z3gyWS"
       KindfulClient.new.import_transaction(@json)
