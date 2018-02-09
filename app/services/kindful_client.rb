c3b36c76519ceac363c25cdc2568424f21011594
Chip Kragt <chiperific@gmail.com>
2018-02-08 22:05:20 -0500

    Ross is so good at Rails

--- app/services/kindful_client.rb
+++ app/services/kindful_client.rb
@@ -12,14 +12,14 @@ class KindfulClient
       fname: user.fname,
       lname: user.lname,
       email: user.email,
-      phone: user.phone,
       email_opt_in: user.email_opt_out ? false : true
     }
     self.class.post("/imports", {headers: headers, body: contact(**body_args).to_json} )
   end
 
-  def import_transaction
-    #causevox
+  def import_transaction(transaction)
+    
+    self.class.post("/imports", { headers: headers, body: contact_w_transaction(transaction).to_json } )
   end
 
   def token
@@ -33,11 +33,45 @@ class KindfulClient
     }
   end
 
-  def contact_w_transaction()
-    #causevox
+  def contact_w_transaction(opts)
+    # opts[:key] is symbolized already, just make sure the names match the charge_succeded_spec keys
+
+    {
+      "data_format": "contact_with_transaction",
+      "action_type": "create",
+      "data_type": "json",
+      "match_by": {
+        "contact": "first_name_last_name_email",
+        "campaign": "name",
+        "fund": "name"
+      },
+      "data": [
+        {
+          "first_name": opts[:first_name],
+          "last_name": opts[:lname],
+          "email": opts[:email],
+          "addr1": opts[:line1],
+          "addr2": opts[:line2],
+          "city": opts[:city],
+          "state": opts[:state],
+          "postal": opts[:zipcode],
+          "country": opts[:country],
+          "amount_in_cents": opts[:amount_in_cents],
+          "currency": "usd",
+          "campaign": "CauseVox Transactions",
+          "fund": "Special Events 40400",
+          "acknowledged": "false",
+          "transaction_note": opts[:campaign_name],
+          "stripe_charge_id": opts[:charge_id],
+          "transaction_type": opts[:funding],
+          "card_type": opts[:card_brand],
+         } 
+      ]
+    }
+
   end
 
-  def contact(id:, fname:, lname:, email:, phone:, email_opt_in:)
+  def contact(id:, fname:, lname:, email:, email_opt_in:)
     {
       "data_format": "contact",
       "action_type": "update",
---
+++ spec/requests/stripe_webhook_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+
+
+RSpec.describe "Stripe Webhook", type: :request do
+  context "successful post from CauseVox" do
+    before :each do
+      data = JSON.parse(file_fixture('charge_succeeded_spec.json').read)
+      post stripe_webhook_path, params: data
+    end
+
+
+    fit "responds with 200 OK" do
+      expect_status :ok
+    end
+
+    fit "parses the JSON" do
+      #write this next
+      expect( assigns(:json) ).to be  #formatted somehow
+      # expect(assigns(:json)[:fname]).to be "the real thing"
+      
+    end
+
+    fit "sends the transaction and user to Kindful" do
+    end
+  end
+
+  context "Post from anyone else" do
+    before :each do
+      data = { "object": { "application": "not_causevox" } }
+      post stripe_webhook_path, params: data
+    end
+
+    fit "responds with 200 OK" do
+      expect_status :ok
+    end
+
+    fit "doesn't pass to Kindful" do
+      expect_any_instance_of(KindfulClient).to_not receive(:import_transaction)
+    end
+  end
+
+end
