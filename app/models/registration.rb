45e5d5b68cd56fa3efb8329c43e9297d2baad1cc
Chip Kragt <chiperific@gmail.com>
2018-02-15 00:05:17 -0500

    all tests passing, good night

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -9,42 +9,6 @@ class Registration < ApplicationRecord
   attr_accessor :accept_waiver
 
   validates :guests_registered, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
-  # validate :under_max_registrations?
-  # validate :under_max_leaders?
-
-  # def under_max_registrations?
-  #   return if leader?
-
-  #   if self.guests_registered == nil
-  #     self.guests_registered = 0
-  #   end
-
-  #   # Diff in count of attendees - if it's a new record, it's the total guest count plus the registrant.
-  #   # If it's an update, it's just the difference in guest count. 
-  #   # Special case if the user registered as a leader then de-registered.
-  #   attendees_diff = new_record? ? guests_registered + 1 : guests_registered - guests_registered_was - (leader_was ? 1 : 0)
-
-  #   # overflow represents how many attendees we would be over max if this registration succeeded
-  #   overflow = event.total_registered + attendees_diff - event.max_registrations
-
-  #   if overflow > 0
-  #     #errors.add(:email, "maximum registrations exceeded by #{overflow} for event")
-  #     self.errors.add(:guests_registered, "maximum registrations exceeded by #{overflow} for event")
-  #     false
-  #   end
-  # end
-
-  # def under_max_leaders?
-  #   return if !leader?
-
-  #   leader_added = leader? && !leader_was
-  #   leader_removed = !leader? && leader_was
-  #   leader_diff = leader_added ? 1 : leader_removed ? -1 : 0
-
-  #   if event.leaders_registered.count + leader_diff > event.max_leaders
-  #     errors.add(:leader, 'maximum leaders exceeded for event')
-  #   end
-  # end
 
   def form_source
     # this allows for a form field that handles page redirects based on values: "admin", "self", "anon"
