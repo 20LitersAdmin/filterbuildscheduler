bdb846a6421af2090bf39c1ff2156db4eb552df7
Chip Kragt <chiperific@gmail.com>
2018-02-27 03:35:49 -0500

    safety check

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,7 +2,16 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def receive
-    @json = params[:data][:object].as_json
+    # Stripe's test webhook doesn't have application or metadata. 
+    # To test to Kindful's sandbox, we need to send the charge_succeeded_spec file.  
+    # So, when in Development and the webhook is received from Stripe, ignore it and use my test file  
+    if Rails.env.development?  
+      file = JSON.parse(File.read("#{Rails.root}/spec/fixtures/files/charge_succeeded_spec.json")) 
+      @json = file["data"]["object"] 
+    else # test and production envs  
+      @json = params[:data][:object].as_json 
+    end
+    
     @json.deep_symbolize_keys!
 
     if @json[:application] == "ca_14yEk8gp5dbBbYDnndXU9yTNM3Z3gyWS"
