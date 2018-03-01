b46f12eb494b9ce0c319644e3bae721bd8d4a612
Chip Kragt <chiperific@gmail.com>
2018-03-01 10:07:16 -0500

    make gcal links be secure

--- app/mailers/mailer_helper.rb
+++ app/mailers/mailer_helper.rb
@@ -5,11 +5,11 @@ module MailerHelper
   end
 
   def url_for_event_gcal
-    "http://www.google.com/calendar/event?action=TEMPLATE&text=#{(@event.title + ': ' + @event.technology.name).gsub(' ','%20')}&dates=#{datetime_for_gcal}&details=#{(@event.privacy_humanize).gsub(' ','%20')}&location=#{(@event.location.addr_one_liner).gsub(' ','%20')}&trp=false&sprop=https://make.20liters.org/&sprop=name:20%20Liters"
+    "https://www.google.com/calendar/event?action=TEMPLATE&text=#{(@event.title + ': ' + @event.technology.name).gsub(' ','%20')}&dates=#{datetime_for_gcal}&details=#{(@event.privacy_humanize).gsub(' ','%20')}&location=#{(@event.location.addr_one_liner).gsub(' ','%20')}&trp=false&sprop=https://make.20liters.org/&sprop=name:20%20Liters"
   end
 
   def url_for_registration_gcal
-    "http://www.google.com/calendar/event?action=TEMPLATE&text=[20%20Liters]%20Filter%20Build&dates=#{datetime_for_gcal}&details=#{@details}&location=#{(@event.location.addr_one_liner).gsub(' ','%20')}&trp=false&sprop=https://make.20liters.org/&sprop=name:20%20Liters"
+    "https://www.google.com/calendar/event?action=TEMPLATE&text=[20%20Liters]%20Filter%20Build&dates=#{datetime_for_gcal}&details=#{@details}&location=#{(@event.location.addr_one_liner).gsub(' ','%20')}&trp=false&sprop=https://make.20liters.org/&sprop=name:20%20Liters"
   end
 
   def format_changed_time_range(start_time, end_time)
