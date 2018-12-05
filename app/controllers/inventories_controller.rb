eb728e92c21e6145b354655da35cf3de4c2c10bc
Chip Kragt <chiperific@gmail.com>
2018-12-05 16:25:08 -0500

    refine lable size and structure

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -163,7 +163,6 @@ class InventoriesController < ApplicationController
     @print_navbar = true
     authorize @inventory = Inventory.latest
     @counts = @inventory.counts.sort_by { |c| [c.group_by_tech, c.name] }
-    @loop = (@counts.count/2.to_f).ceil 
   end
 
   def destroy
