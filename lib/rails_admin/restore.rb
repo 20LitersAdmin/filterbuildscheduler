344c7d411fd73c9db7d45dd44d59f9ae4eed40ec
Chip Kragt <chiperific@gmail.com>
2019-02-10 12:26:42 -0500

    custom admin actions for dealing with paranoid

---
+++ lib/rails_admin/restore.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_admin/config/actions'
+require 'rails_admin/config/actions/base'
+
+module RailsAdmin
+  module Config
+    module Actions
+      class Restore < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+
+        register_instance_option :visible? do
+          %w[Component Count Event Inventory Location Material Part Registration Supplier Technology User].include?(bindings[:object].class.to_s) && bindings[:object].deleted?
+        end
+
+        register_instance_option :member do
+          true
+        end
+
+        register_instance_option :link_icon do
+          'icon-circle-arrow-up'
+        end
+
+        register_instance_option :controller do
+          proc do
+            @object.restore(recursive: true)
+            flash[:success] = t('admin.flash.successful', name: @model_config.label, action: t('admin.actions.restore.done'))
+            redirect_to back_or_index
+          end
+        end
+      end
+    end
+  end
+end
