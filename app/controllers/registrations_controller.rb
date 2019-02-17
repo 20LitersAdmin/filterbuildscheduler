6caaa9296abef7d48d67855fd36c5126c53478c0
Chip Kragt <chiperific@gmail.com>
2019-02-17 16:02:42 -0500

    use params to set @registration.leader == true when navigating from events/lead, add owner_acronym to tech table on inventories#status, rubocop registrations_controller, rubocop _registration_registered

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,7 +20,7 @@ class RegistrationsController < ApplicationController
     @event = Event.find(params[:event_id])
 
     case params[:registration][:form_source]
-    when "admin"
+    when 'admin'
       @user = find_or_initialize_user(user_params)
       @user.signed_waiver_on ||= Time.now
 
@@ -32,7 +32,7 @@ class RegistrationsController < ApplicationController
         new_max = @event.total_registered + guests + 1
         @event.update(max_registrations: new_max)
       end
-    when "self"
+    when 'self'
       @user = current_user
       @leader = params[:registration][:leader] || false
       @duplicate_registration_risk = false
@@ -48,34 +48,25 @@ class RegistrationsController < ApplicationController
     # A user who exists but isn't signed in shouldn't be able to register for an event more than once.
     if @duplicate_registration_risk
       registration = Registration.where(event: @event, user: @user)
-      if registration.exists?
-        @registration.errors.add(:email, "This email address has already registered for this event.")
-      end
+      @registration.errors.add(:email, 'This email address has already registered for this event.') if registration.exists?
     end
 
-    if registration_params[:leader] == "1" && !@user.can_lead_event?(@event)
-      @registration.errors.add(:fname, "This user isn't qualified to lead this event.")
-    end
+    @registration.errors.add(:fname, 'This user isn\'t qualified to lead this event.') if registration_params[:leader] == '1' && !@user.can_lead_event?(@event)
 
-    if waiver_accepted == '0'
-      @registration.errors.add(:accept_waiver, "You must review and sign the Liability Waiver first")
-    end
+    @registration.errors.add(:accept_waiver, 'You must review and sign the Liability Waiver first') if waiver_accepted == '0'
 
-    if @event.max_registrations < (@event.total_registered + params[:registration][:guests_registered].to_i + 1) #count up the totals and validate
-      @registration.errors.add(:guests_registered, "You can only bring up to #{@event.registrations_remaining - 1} guests at this event.")
-    end
+    # count up the totals and validate
+    @registration.errors.add(:guests_registered, "You can only bring up to #{@event.registrations_remaining - 1} guests at this event.") if @event.max_registrations < (@event.total_registered + params[:registration][:guests_registered].to_i + 1)
 
     if @registration.errors.blank? && @user.save && @registration.save
-
       sign_in(@user) unless current_user
 
-      if @event.start_time > Time.now # don't send emails for past events.
-        RegistrationMailer.delay.created @registration
-      end
-      @user.update_attributes!(signed_waiver_on: Time.now) unless @registration.waiver_accepted?
-      flash[:success] = "Registration successful!"
+      RegistrationMailer.delay.created(@registration) if @event.start_time > Time.now # don't send emails for past events.
 
-      if params[:registration][:form_source] == "admin"
+      @user.update_attributes!(signed_waiver_on: Time.now) unless @registration.waiver_accepted?
+      flash[:success] = 'Registration successful!'
+
+      if params[:registration][:form_source] == 'admin'
         if params[:commit_and_new].present?
           redirect_to new_event_registration_path @event
         else
@@ -85,9 +76,9 @@ class RegistrationsController < ApplicationController
         redirect_to event_path @event
       end
     else
-      flash[:danger] = @registration.errors.messages.map { |k,v| v }.join(', ')
-      flash[:danger] += @user.errors.messages.map { |k,v| "#{k} #{v.join(', ')}" }.join(' | ')
-      if params[:registration][:form_source] == "admin"
+      flash[:danger] = @registration.errors.messages.map { |_k, v| v }.join(', ')
+      flash[:danger] += @user.errors.messages.map { |k, v| "#{k} #{v.join(', ')}" }.join(' | ')
+      if params[:registration][:form_source] == 'admin'
         render 'new'
       else
         render 'events/show'
@@ -98,11 +89,7 @@ class RegistrationsController < ApplicationController
   def edit
     authorize @registration
 
-    if params[:admin] == "true"
-      @btn_admin = true
-    else
-      @btn_admin = false
-    end
+    @btn_admin = params[:admin] == 'true'
   end
 
   def update
@@ -227,9 +214,10 @@ class RegistrationsController < ApplicationController
   end
 
   def find_or_initialize_registration(user, event)
-        # check for a deleted record before creating a new one.
-    if Registration.with_deleted.where(user: user, event: event).exists?
-      registration = Registration.with_deleted.where(user: user, event: event).first
+    # check for a deleted record before creating a new one.
+    registration_check = Registration.with_deleted.where(user: user, event: event)
+    if registration_check.exists?
+      registration = registration_check.first
       registration.restore
     else
       registration = Registration.new(event: event, user: user)
