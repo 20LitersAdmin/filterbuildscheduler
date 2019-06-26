607f6ab66d2b25189ec2fd1d50d50b7c0d5b5df0
Chip Kragt <chiperific@gmail.com>
2019-06-26 14:11:40 -0400

    add  to Component, Part, Material; add checkbox for ignoring available component counts; calculate available quantity from latest count; NEEDED: extrap_tech_materials partial for any related materials

--- app/models/component.rb
+++ app/models/component.rb
@@ -15,8 +15,16 @@ class Component < ApplicationRecord
   scope :active, -> { where(deleted_at: nil) }
   scope :required, -> { joins(:extrapolate_technology_components).where.not(completed_tech: true).where(extrapolate_technology_components: {required: true }) }
 
-  def uid
-    'C' + id.to_s.rjust(3, '0')
+  def available
+    if latest_count.present?
+      latest_count.available
+    else
+      0
+    end
+  end
+
+  def latest_count
+    Count.where(inventory: Inventory.latest_completed, component: self).first
   end
 
   def picture
@@ -27,33 +35,11 @@ class Component < ApplicationRecord
     end
   end
 
-  def technology
-    technologies.first
-  end
-
   def per_technology
     if extrapolate_technology_components.any?
       extrapolate_technology_components.first.components_per_technology.to_i
     else
       1
-    end
-  end
-
-  def required?
-    if extrapolate_technology_components.any?
-      extrapolate_technology_components.first.required?
-    else
-      false
-    end
-  end
-
-  def total
-    count = Count.where(inventory: Inventory.latest_completed, component: self).first
-
-    if count
-      count.total
-    else
-      0
     end
   end
 
@@ -70,5 +56,29 @@ class Component < ApplicationRecord
       end
     end
     ary.sum
+  end
+
+  def required?
+    if extrapolate_technology_components.any?
+      extrapolate_technology_components.first.required?
+    else
+      false
+    end
+  end
+
+  def technology
+    technologies.first
+  end
+
+  def total
+    if latest_count
+      latest_count.total
+    else
+      0
+    end
+  end
+
+  def uid
+    'C' + id.to_s.rjust(3, '0')
   end
 end
