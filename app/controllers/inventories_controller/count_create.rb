5acfe07a82a8e84b33c3cda6c39a3a1f3802a9b1
Chip Kragt <chiperific@gmail.com>
2019-12-17 13:39:18 -0500

    inventories can bypass certain counts by technology

--- app/controllers/inventories_controller/count_create.rb
+++ app/controllers/inventories_controller/count_create.rb
@@ -2,61 +2,85 @@
 
 class InventoriesController
   class CountCreate
-    def initialize(inventory)
+    def initialize(inventory, technology_params, current_user)
       @inventory = inventory
       # Is there a previous inventory?
       @latest = Inventory.former.first
-      if @latest.present?
-        @latest_id = @latest.id
-      else
-        @latest_id = nil
-      end
+      @latest_id = @latest.present? ? @latest.id : nil
 
+      @tech_ids = technology_params.present? ? technology_params['technologies'].map(&:to_i) : []
+      @user_id = current_user.id
+
+      make_parts
+      make_materials
+      make_components
+    end
+
+    def make_parts
       ### Make all the parts, use the previous Inventory's values if present.
-      @part_ids = Part.all.map { |o| o.id }
-      @part_ids.each do |p|
-        old_part_count = Count.where(inventory_id: @latest_id).where(part_id: p).last
-        if old_part_count.present?
-          new_part_count = old_part_count.dup
-          new_part_count.inventory_id = @inventory.id
-          new_part_count.user_id = nil
-          new_part_count.extrapolated_count = 0
-          new_part_count.save
-        else
-          Count.create(inventory_id: @inventory.id, part_id: p)
+      @part_ids = Part.all.pluck(:id)
+      @part_ids.each do |pt|
+        old_part_count = Count.where(inventory_id: @latest_id).where(part_id: pt).last
+        part_count = old_part_count.present? ? duplicate(old_part_count) : create_part(pt)
+
+        if @tech_ids.any?
+          bypass(part_count) if @tech_ids.include?(part_count.technology&.id)
         end
       end
+    end
 
+    def make_materials
       ### Make all the materials, use the previous Inventory's values if present.
-      @material_ids = Material.all.map { |o| o.id }
+      @material_ids = Material.all.pluck(:id)
       @material_ids.each do |m|
         old_material_count = Count.where(inventory_id: @latest_id).where(material_id: m).last
-        if old_material_count.present?
-          new_material_count = old_material_count.dup
-          new_material_count.inventory_id = @inventory.id
-          new_material_count.user_id = nil
-          new_material_count.extrapolated_count = 0
-          new_material_count.save
-        else
-          Count.create(inventory_id: @inventory.id, material_id: m)
+        material_count = old_material_count.present? ? duplicate(old_material_count) : create_material(m)
+
+        if @tech_ids.any?
+          bypass(material_count) if @tech_ids.include?(material_count.technology&.id)
         end
       end
+    end
 
+    def make_components
       ### Make all the components, use the previous Inventory's values if present.
-      @component_ids = Component.all.map { |o| o.id }
+      @component_ids = Component.all.pluck(:id)
       @component_ids.each do |c|
         old_component_count = Count.where(inventory_id: @latest_id).where(component_id: c).last
-        if old_component_count.present?
-          new_component_count = old_component_count.dup
-          new_component_count.inventory_id = @inventory.id
-          new_component_count.user_id = nil
-          new_component_count.extrapolated_count = 0
-          new_component_count.save
-        else
-          Count.create(inventory_id: @inventory.id, component_id: c)
+        component_count = old_component_count.present? ? duplicate(old_component_count) : create_component(c)
+
+        if @tech_ids.any?
+          bypass(component_count) if @tech_ids.include?(component_count.technology&.id)
         end
       end
+    end
 
+    def duplicate(old_count)
+      new_count = old_count.dup
+      new_count.tap do |c|
+        c.inventory_id = @inventory.id
+        c.extrapolated_count = 0
+        c.user_id = nil
+        c.save
+      end
+
+      new_count.reload
+    end
+
+    def create_part(id)
+      Count.where(inventory_id: @inventory.id, part_id: id).first_or_create
+    end
+
+    def create_material(id)
+      Count.where(inventory_id: @inventory.id, material_id: id).first_or_create
+    end
+
+    def create_component(id)
+      Count.where(inventory_id: @inventory.id, component_id: id).first_or_create
+    end
+
+    def bypass(count)
+      count.update(user_id: @user_id)
     end
   end
 end
