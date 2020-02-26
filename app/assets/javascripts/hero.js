3316f4e21c91f62e7e19b15fe529cdd8110c4f2d
Chip Kragt <chiperific@gmail.com>
2020-02-26 16:28:24 -0500

    fix human_number handling strings, make basic_stat_block responsive with JS

---
+++ app/assets/javascripts/hero.js
@@ -0,0 +1,34 @@
+function sizeStatsBlocks() {
+  var statBlocks = $('.stat-block');
+  var groupSize;
+
+  if (window.innerWidth > 920) {
+    groupSize = 6;
+  } else {
+    groupSize = 3;
+  };
+  var groups = statBlocks.length/groupSize;
+  var fullRows = Math.floor(groups);
+  var breakPoint = (fullRows * groupSize) - 1;
+  var remainder = statBlocks.length - (groupSize * fullRows);
+  var groupWidth = 100/groupSize + "%";
+  var remainderWidth = 100/remainder + "%";
+
+  statBlocks.each(function(index) {
+    if (index <= breakPoint) {
+      $(this).css('width', groupWidth);
+    } else {
+      $(this).css('width', remainderWidth);
+    };
+  });
+};
+
+$(document).on("turbolinks:load", function(){
+  if ((controllerMatches('events') && actionMatches('index'))) {
+    sizeStatsBlocks();
+
+    $(window).on("resize", function(){
+      sizeStatsBlocks();
+    });
+  }
+});
