84869b25cb4534f32d38e357acbf4d5d8884374d
Chip Kragt <chiperific@gmail.com>
2020-11-08 21:54:27 -0500

    migration, model, controller, and policy for OauthUser, service for GmailClient w/ notes, add gems: Omniauth, GoogleAPIClient, write Omniauth config to application.rb

---
+++ app/controllers/oauth_users_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class OauthUsersController < ApplicationController
+  def in
+    if session[:oauth_user_id].present?
+      flash[:notice] = 'Already authenticated.'
+      redirect_to auth_status_path(session[:oauth_user_id])
+    end
+  end
+
+  def callback
+    auth = request.env['omniauth.auth']
+
+    oauth_user = OauthUser.from_omniauth(auth)
+    session[:oauth_user_id] = oauth_user.id
+
+    flash[:notice] = 'Successful authentication!'
+    redirect_to auth_status_path(oauth_user.id)
+  end
+
+  def out
+    session[:oauth_user_id] = nil
+  end
+
+  def failure
+    flash[:alert] = "Authentication error: #{params[:message].humanize}"
+    redirect_to in_path
+  end
+
+  def status
+    @oauth_user = OauthUser.find(params[:id])
+  end
+end
---
+++ app/models/oauth_user.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class OauthUser < ApplicationRecord
+  validates :oauth_id, :oauth_token, uniqueness: true, allow_blank: true
+
+  def self.from_omniauth(auth)
+    @oauth_user = where(email: auth.info.email).first_or_initialize.tap do |user|
+      user.name = auth.info.name
+      user.oauth_provider = auth.provider
+      user.oauth_id = auth.uid
+      user.oauth_image_link = auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_refresh_token ||= auth.credentials.refresh_token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    end
+
+    @oauth_user.save
+    @oauth_user.reload
+  end
+
+  def authorization
+    Google::APIClient::ClientSecrets.new(
+      {
+        'web' =>
+          {
+            'access_token' => oauth_token,
+            'refresh_token' => oauth_refresh_token,
+            'client_id' => Rails.application.credentials.google_client_id,
+            'client_secret' => Rails.application.credentials.google_client_secret
+          }
+      }
+    ).to_authorization
+  end
+
+  def oauth_expired?
+    oauth_expires_at < Time.now
+  end
+
+  def email_service
+    # https://github.com/googleapis/google-api-ruby-client/blob/master/generated/google/apis/gmail_v1/service.rb
+    service = Google::Apis::GmailV1::GmailService.new
+    service.authorization = authorization
+
+    if oauth_expired?
+      response = service.authorization.refresh!
+      new_expiry = Time.now + response['expires_in']
+      update_column(:oauth_expires_at, new_expiry)
+    end
+
+    service
+  end
+end
