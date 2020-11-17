64391eea854674f8e1a0c9f4667af6dc0a56c1b7
Chip Kragt <chiperific@gmail.com>
2020-11-17 16:16:26 -0500

    temp comment out lines on latest migration

--- db/migrate/20201116040358_change_oauth_users_last_email_sync_to_date_time.rb
+++ db/migrate/20201116040358_change_oauth_users_last_email_sync_to_date_time.rb
@@ -3,11 +3,11 @@
 class ChangeOauthUsersLastEmailSyncToDateTime < ActiveRecord::Migration[6.0]
   def up
     change_column :oauth_users, :last_email_sync, :datetime
-    add_column :oauth_users, :oauth_error_message, :string
+    # add_column :oauth_users, :oauth_error_message, :string
   end
 
   def down
     change_column :oauth_users, :last_email_sync, :date
-    remove_column :oauth_users, :oauth_error_message, :string
+    # remove_column :oauth_users, :oauth_error_message, :string
   end
 end
