fb94a36f235ed652057f3a0d4839f82c6ae8c7a0
Chip Kragt <chiperific@gmail.com>
2021-03-01 14:48:53 -0500

    create Organizations table

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_162228) do
+ActiveRecord::Schema.define(version: 2021_03_01_194751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -238,6 +238,15 @@ ActiveRecord::Schema.define(version: 2021_01_28_162228) do
     t.index ["email"], name: "index_oauth_users_on_email", unique: true
     t.index ["oauth_id"], name: "index_oauth_users_on_oauth_id", unique: true
     t.index ["oauth_token"], name: "index_oauth_users_on_oauth_token", unique: true
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_organizations_on_email"
+    t.index ["name"], name: "index_organizations_on_name"
   end
 
   create_table "parts", force: :cascade do |t|
