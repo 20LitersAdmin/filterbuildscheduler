243befe89c3807f8f6196958ae36b7de25601453
Chip Kragt <chiperific@gmail.com>
2021-03-11 10:17:40 -0500

    add COVID language to registration emails, add plain text versions of registrations, fix a bit of grammer.

--- spec/mailers/previews/registration_mailer_preview.rb
+++ spec/mailers/previews/registration_mailer_preview.rb
@@ -3,29 +3,29 @@
 # Preview all emails at http://localhost:3000/rails/mailers/registration_mailer
 class RegistrationMailerPreview < ActionMailer::Preview
   def created
-    RegistrationMailer.created(Registration.first)
+    RegistrationMailer.created(Registration.last)
   end
 
   def reminder
-    RegistrationMailer.reminder(Registration.first)
+    RegistrationMailer.reminder(Registration.last)
   end
 
   def event_changed
-    event = Event.first
-    event.start_time = DateTime.new(2017, 11, 8, 16, 0, 0, '-05:00')
-    event.end_time = DateTime.new(2017, 11, 8, 21, 0, 0, '-05:00')
+    event = Event.last
+    event.start_time = Time.new(2017, 11, 8, 16, 0, 0, '-05:00')
+    event.end_time = Time.new(2017, 11, 8, 21, 0, 0, '-05:00')
     event.technology_id = 2
     event.location_id = 2
     event.is_private = true
 
-    RegistrationMailer.event_changed(Registration.first, event)
+    RegistrationMailer.event_changed(Registration.last, event)
   end
 
   def event_cancelled
-    RegistrationMailer.event_cancelled(Registration.first.id)
+    RegistrationMailer.event_cancelled(Registration.last.id)
   end
 
   def event_results
-    RegistrationMailer.event_results(Registration.first)
+    RegistrationMailer.event_results(Registration.last)
   end
 end
