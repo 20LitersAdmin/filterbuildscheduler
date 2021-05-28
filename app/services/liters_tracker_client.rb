3e46cbf3ea1fa32726a0e123cd108793d7fa4729
Chip Kragt <chiperific@gmail.com>
2021-05-28 16:12:22 -0400

    deal with LitersTrackerClient failing due to SSL error

--- app/services/liters_tracker_client.rb
+++ app/services/liters_tracker_client.rb
@@ -8,10 +8,14 @@ class LitersTrackerClient
   base_uri 'https://track.20liters.org/stats'
 
   def initialize
-    response = HTTParty.get('https://track.20liters.org/stats')
+    begin
+      response = HTTParty.get('https://track.20liters.org/stats')
 
-    @as_of_date = Date.parse response.parsed_response.pop['as_of_date']
+      @as_of_date = Date.parse response.parsed_response.pop['as_of_date']
 
-    @stat_ary = response.parsed_response.compact.map(&:symbolize_keys)
+      @stat_ary = response.parsed_response.compact.map(&:symbolize_keys)
+    rescue OpenSSL::SSL::SSLError
+      @stat_ary = []
+    end
   end
 end
