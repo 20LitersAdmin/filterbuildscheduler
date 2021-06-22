6b81f572f243241f66e6c481eb265703f8ea062a
Chip Kragt <chiperific@gmail.com>
2021-06-21 22:32:46 -0400

    layout problems & solutions in README, begin designing migrations

---
+++ db/migrate/20210620173938_add_counts_to_item_tables.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+class AddCountsToItemTables < ActiveRecord::Migration[6.1]
+  def change
+    # add_column :materials,    :loose_count,          :integer, default: 0
+    # add_column :materials,    :box_count,            :integer, default: 0
+    # add_column :materials,    :loose_count_previous, :integer, default: 0
+    # add_column :materials,    :box_count_previous,   :integer, default: 0
+    # add_column :materials,    :count_date,           :datetime, index: true
+    # add_column :materials,    :count_date_previous,  :datetime, index: true
+
+    # add_column :parts,        :loose_count,          :integer, default: 0
+    # add_column :parts,        :box_count,            :integer, default: 0
+    # add_column :parts,        :loose_count_previous, :integer, default: 0
+    # add_column :parts,        :box_count_previous,   :integer, default: 0
+    # add_column :parts,        :count_date,           :datetime, index: true
+    # add_column :parts,        :count_date_previous,  :datetime, index: true
+
+    # add_column :components,   :loose_count,          :integer, default: 0
+    # add_column :components,   :box_count,            :integer, default: 0
+    # add_column :components,   :loose_count_previous, :integer, default: 0
+    # add_column :components,   :box_count_previous,   :integer, default: 0
+    # add_column :components,   :count_date,           :datetime, index: true
+    # add_column :components,   :count_date_previous,  :datetime, index: true
+
+    # add_column :technologies, :loose_count,          :integer, default: 0
+    # add_column :technologies, :box_count,            :integer, default: 0
+    # add_column :technologies, :loose_count_previous, :integer, default: 0
+    # add_column :technologies, :box_count_previous,   :integer, default: 0
+    # add_column :technologies, :count_date,           :datetime, index: true
+    # add_column :technologies, :count_date_previous,  :datetime, index: true
+    # # Allows for filtering out of technologies that shouldn't be inventoried
+    # add_column :technologies, :inventoryable,        :boolean, default: true, index: true
+
+    # # add counts from latest inventory
+    # inv = Inventory.latest
+    # latest_date = inv.date
+    # inv.counts.each do |c|
+    #   c.item.update_columns(
+    #     loose_count: c.loose_count,
+    #     box_count: c.unopened_boxes_count,
+    #     count_date: latest_date
+    #   )
+    # end
+
+    # # add counts from previous inventory
+    # prev_inv = Inventory.former.first
+    # prev_date = prev_inv.date
+    # prev_inv.counts.each do |c|
+    #   c.item.update_columns(
+    #     loose_count_previous: c.loose_count,
+    #     box_count_previous: c.unopened_boxes_count,
+    #     count_date_previous: prev_date
+    #   )
+    # end
+
+    # # transfer counts from Components.where(completed_tech: true) to Technology
+    # Component.where(completed_tech: true).each do |comp|
+    #   tech = comp.technology
+    #   tech.update_columns(
+    #     loose_count: comp.loose_count,
+    #     box_count: comp.box_count,
+    #     loose_count_previous: comp.loose_count_previous,
+    #     box_count_previous: comp.box_count_previous,
+    #     count_date: comp.count_date,
+    #     count_date_previous: comp.count_date_previous
+    #   )
+    # end
+
+    # # Count: switch from booleans to polymorphic
+    # # count.item_type && count.item_id
+    # add_reference :counts, :item, polymorphic: true, index: true
+  end
+end
--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -5,26 +5,26 @@ require 'rails_helper'
 RSpec.describe Material, type: :model do
   let(:material) { create :material }
 
-  describe "must be valid" do
+  describe 'must be valid' do
     let(:no_name) { build :material, name: nil }
     let(:no_price) { build :material, price_cents: nil }
     let(:no_minimum) { build :material, minimum_on_hand: nil }
 
     let(:negative_price) { build :material, price_cents: -300 }
 
-    it "in order to save" do
+    it 'in order to save' do
       expect(material.save).to eq true
       expect { no_name.save!(validate: false) }.to raise_error ActiveRecord::NotNullViolation
       expect { no_price.save!(validate: false) }.to raise_error ActiveRecord::NotNullViolation
       expect { no_minimum.save!(validate: false) }.to raise_error ActiveRecord::NotNullViolation
     end
 
-    it "prices must be positive" do
+    it 'prices must be positive' do
       expect(negative_price.save).to be_falsey
     end
   end
 
-  describe "#per_technology" do
+  describe '#per_technology' do
     let(:part) { create :part }
     let(:material_part) { create :material_part, material: material, part: part, parts_per_material: 10 }
 
@@ -34,11 +34,11 @@ RSpec.describe Material, type: :model do
     let(:technology) { create :technology }
     let(:tech_comp) { create :tech_comp, component: component, technology: technology, components_per_technology: 2 }
 
-    it "returns 0 if there is no part relationship" do
+    it 'returns 0 if there is no part relationship' do
       expect(material.per_technology).to eq 0
     end
 
-    it "returns a float that represents the number of materials in a complete technology" do
+    it 'returns a float that represents the number of materials in a complete technology' do
       material
       part
       component
