646babdacdde95d11b2b8de0a8f2eeca34f2cdb4
Chip Kragt <chiperific@gmail.com>
2021-09-12 13:27:23 -0400

    RailsAdmin js sets body data- attributes

--- app/assets/javascripts/rails_admin/custom/rails_admin_global.js
+++ app/assets/javascripts/rails_admin/custom/rails_admin_global.js
@@ -2,6 +2,12 @@ function isRailsAdminPage() {
   return $('body').hasClass('rails_admin');
 };
 
+function isRailsAdminDashboard() {
+  // check for path '/admin'
+  // don't rely on <body data-action='dashboard'> because this can fail to be updated when calls are AJAXed
+  return window.location.pathname.split('/').join('') == 'admin'
+}
+
 function controllerMatches(controllers_ary) {
   var controller = $('body').data('controller');
   return controllers_ary.indexOf(controller) !== -1;
@@ -12,31 +18,34 @@ function actionMatches(actions_ary) {
   return actions_ary.indexOf(action) !== -1;
 };
 
-// ajaxComplete calls need to update <body data-controller="" data-action=""> values so that rails_admin/custom/*.js functions can use controllerMatches() and actionMatches() confidently
-$(document).ajaxComplete( function() {
-  // I don't think these JS files are accessible outside of RailsAdmin, but to be safe, wrap everything in a check to see if we're inside RailsAdmin
-  if ( isRailsAdminPage() ) {
-    // If <body> has no data-controller set, we can assume we are on the '/admin' dashboard page, data-action will be 'dashboard'
-    if ( $('body').data('controller') != undefined ) {
-      dataAction = $('body').data('action');
-      pathAry = window.location.pathname.split('/');
-      lastItem = pathAry[pathAry.length -1];
+function setBodyDataTags() {
+  // I don't think these JS files are accessible outside of RailsAdmin, but to be safe, wrap everything in a check to see if we're inside RailsAdmin and not on the dashboard
+  if ( isRailsAdminPage() && !isRailsAdminDashboard() ) {
+    dataAction = $('body').data('action');
+    pathAry = window.location.pathname.split('/');
+    lastItem = pathAry[pathAry.length -1];
 
-      if ( (lastItem == 'edit') && (dataAction != 'edit') ) {
-        $('body').data('action', 'edit');
-      } else if ( (lastItem == 'new') && (dataAction != 'new') ) {
-        $('body').data('action', 'new');
-      } else if ( ($.isNumeric(lastItem)) && (dataAction != 'show') ) {
-        // technically ($.isNumeric(lastItem) will return true for other actions [create, delete, restore], but since they don't have views, it's fine to pretend we're on the show view.
-        $('body').data('action', 'show');
-      } else {
-        // Else lastItem can be assumed to be a model name and therefore data-action should be 'index'
-        dataController = $('body').data('controller');
-        if ( lastItem != dataController ) {
-          $('body').data('controller', lastItem);
-          $('body').data('action', 'index');
-        };
+    if ( (lastItem == 'edit') && (dataAction != 'edit') ) {
+      $('body').data('action', 'edit');
+    } else if ( (lastItem == 'new') && (dataAction != 'new') ) {
+      $('body').data('action', 'new');
+    } else if ( ($.isNumeric(lastItem)) && (dataAction != 'show') ) {
+      // technically ($.isNumeric(lastItem) will return true for other actions [create, delete, restore], but since they don't have views, it's fine to pretend we're on the show view.
+      $('body').data('action', 'show');
+    } else {
+      // Else lastItem can be assumed to be a model name and therefore data-action should be 'index'
+      dataController = $('body').data('controller');
+      if ( lastItem != dataController ) {
+        $('body').data('controller', lastItem);
+        $('body').data('action', 'index');
       };
     };
   };
+};
+
+// Ensure <body> tag always has data attributes: data-controller && data-action
+// views/layouts/rails_admin/application.html.haml sets the data values for full page loads
+// ajaxComplete calls need to update data attributes so that rails_admin/custom/*.js functions can use controllerMatches() and actionMatches() confidently
+$(document).ajaxComplete( function() {
+  setBodyDataTags();
 });
