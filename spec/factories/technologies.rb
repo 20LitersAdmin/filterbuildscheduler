3d2c181214c562ae2926c07e8d87a60f14107c2b
Chip Kragt <chiperific@gmail.com>
2021-09-14 22:46:16 -0400

    write ApplicationHelper tests

--- spec/factories/technologies.rb
+++ spec/factories/technologies.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :technology do
     name { Faker::Beer.name }
+    short_name { name[0..4] }
     description { Faker::TvShows::TwinPeaks.quote }
     ideal_build_length { 2 }
     ideal_group_size { 20 }
