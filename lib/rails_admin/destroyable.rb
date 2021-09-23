488569e57c1cc6bd2c15e0773d05f8a774758458
Chip Kragt <chiperific@gmail.com>
2021-09-23 09:09:49 -0400

    parity among RailsAdmin custom action names

--- lib/rails_admin/destroyable.rb
+++ lib/rails_admin/destroyable.rb
@@ -6,7 +6,7 @@ require 'rails_admin/config/actions/base'
 module RailsAdmin
   module Config
     module Actions
-      class Destroy < RailsAdmin::Config::Actions::Base
+      class Destroyable < RailsAdmin::Config::Actions::Base
         RailsAdmin::Config::Actions.register(self)
 
         register_instance_option :visible? do
