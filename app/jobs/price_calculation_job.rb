4927f8eb8b3bcf0bf5148bddcd9de3a7b3579dc7
Chip Kragt <chiperific@gmail.com>
2021-11-08 00:36:29 -0500

    Part: #set_made_from_material now checks #quantity_from_material; PriceCalculationJob: now sets prices for Part#made_from_material as well

--- app/jobs/price_calculation_job.rb
+++ app/jobs/price_calculation_job.rb
@@ -8,14 +8,30 @@ class PriceCalculationJob < ApplicationJob
 
     puts '========================= Starting PriceCalculationJob ========================='
 
-    puts 'Setting Technology and Component prices to 0'
+    puts 'Setting Technology, Component, and Part#made_from_material prices to 0'
     # skip callbacks to avoid an infinite loop of triggering this job
     Technology.update_all(price_cents: 0)
     Component.update_all(price_cents: 0)
+    Part.made_from_material.update_all(price_cents: 0)
+
+    # recalculate Part#made_from_material before looping over assemblies
+    puts 'Setting prices for Parts made from Materials'
+    Part.made_from_material.each do |part|
+      q_from_m = part.quantity_from_material
+      if q_from_m.zero? || q_from_m.nil?
+        puts "*** #{part.uid} has no quantity_from_material!!! It was skipped ***"
+        next
+      end
+
+      # round up to the nearest cent using .ceil()
+      new_price_cents = (part.material.price_cents / part.quantity_from_material).ceil
+
+      part.update_columns(price_cents: new_price_cents)
+    end
 
     # starting with the "lowest" nodes is crucial to make sure prices
     # get summed as we traverse upwards towards the roots
-    puts 'Looping over Assemblies'
+    puts 'Looping over Assemblies to set their prices and their combination\'s price'
     Assembly.descending.each do |a|
       # reclaculates a.price_cents on save
       a.save
@@ -23,6 +39,7 @@ class PriceCalculationJob < ApplicationJob
 
       c = a.combination
       # skip callbacks to avoid an infinite loop of triggering this job
+      # c.price_cents + a.price_cents is sorta just +=
       c.update_columns(price_cents: c.price_cents + a.price_cents)
     end
 
