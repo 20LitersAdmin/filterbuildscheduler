6375f02acbcf033ae0f6ce5485ab61971ae474de
Chip Kragt <chiperific@gmail.com>
2021-11-22 12:12:35 -0500

    Supplier is spec'd

--- spec/factories/parts.rb
+++ spec/factories/parts.rb
@@ -6,12 +6,10 @@ FactoryBot.define do
     supplier
     price_cents { Random.rand(7..2900) }
     minimum_on_hand { Random.rand(10..2000) }
-  end
 
-  factory :part_from_material, class: Part do
-    name { Faker::Beer.name }
-    material
-    quantity_from_material { 0.25 }
-    minimum_on_hand { Random.rand(10..2000) }
+    factory :part_from_material do
+      material
+      quantity_from_material { 0.25 }
+    end
   end
 end
