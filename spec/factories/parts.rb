d8836d30f6042ca10dd97586db6c8425cd62030e
Chip Kragt <chiperific@gmail.com>
2021-11-24 23:12:31 -0500

    Model and Part factories can't initialize with price_cents or it screws with Itemable#run_price_calculation_job specs

--- spec/factories/parts.rb
+++ spec/factories/parts.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :part do
     name { Faker::Beer.name }
     supplier
-    price_cents { Random.rand(7..2900) }
+    # price_cents { Random.rand(7..2900) }
     minimum_on_hand { Random.rand(10..2000) }
 
     factory :part_from_material do
