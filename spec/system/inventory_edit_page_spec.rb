fae800b4299cdec3c4255e6ebaad439141ee172a
Chip Kragt <chiperific@gmail.com>
2021-11-28 21:16:10 -0500

    working on inventory_edit_page_spec

--- spec/system/inventory_edit_page_spec.rb
+++ spec/system/inventory_edit_page_spec.rb
@@ -2,161 +2,200 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Inventory edit page', type: :system, js: true do
-  before :each do
-    @inventory = FactoryBot.create(:inventory)
-    supplier = FactoryBot.create(:supplier)
+RSpec.describe 'Inventory edit page', type: :system do
+  let(:inventory) { create :inventory }
 
-    @tech1 = FactoryBot.create(:technology, name: 'Tech #1')
-    @tech2 = FactoryBot.create(:technology, name: 'Tech #2')
+  context 'when there are no counts' do
+    it 'redirects to show page' do
+      sign_in create(:admin)
+      visit edit_inventory_path inventory
 
-    2.times { FactoryBot.create(:component_ct) }
-    3.times { FactoryBot.create(:component) }
-    Component.where('id % 2 = ?', '0').each do |c|
-      FactoryBot.create(:tech_comp, component: c, technology: @tech1)
+      expect(page).to have_content "#{inventory.name} Inventory"
+      expect(current_path).to eq inventory_path(inventory)
     end
-
-    Component.where('id % 2 = ?', '1').each do |c|
-      FactoryBot.create(:tech_comp, component: c, technology: @tech2)
-    end
-
-    4.times { FactoryBot.create(:part, supplier: supplier) }
-    Part.where('id % 2 = ?', '0').each do |part|
-      FactoryBot.create(:tech_part, part: part, technology: @tech1)
-    end
-
-    Part.where('id % 2 = ?', '1').each do |part|
-      FactoryBot.create(:tech_part, part: part, technology: @tech2)
-    end
-
-    @user = FactoryBot.create(:admin)
-
-    InventoriesController::CountCreate.new(@inventory, [], @user)
-  end
-
-  after :each do
-    clean_up!
   end
 
   context 'when visited by' do
+    let(:count) { create :count, inventory: inventory }
+
     it 'anon users redirects to sign_in page' do
-      visit edit_inventory_path @inventory
+      count
+      visit edit_inventory_path inventory
 
       expect(page).to have_content 'You need to sign in first'
       expect(page).to have_content 'Sign in'
     end
 
     it 'builders redirects to home page' do
-      sign_in FactoryBot.create(:user)
-      visit edit_inventory_path @inventory
+      count
+      sign_in create(:user)
+      visit edit_inventory_path inventory
 
       expect(page).to have_content "You don't have permission"
       expect(page).to have_content 'Upcoming Builds'
     end
 
-    it 'inventory users shows the page' do
-      sign_in FactoryBot.create(:user, does_inventory: true)
-      visit edit_inventory_path @inventory
+    it 'leaders redirects to home page' do
+      count
+      sign_in create(:leader)
+      visit edit_inventory_path inventory
 
-      expect(page).to have_content @inventory.name
+      expect(page).to have_content "You don't have permission"
+      expect(page).to have_content 'Upcoming Builds'
     end
 
-    it 'users who receive inventory emails shows the page' do
-      sign_in FactoryBot.create(:user, send_inventory_emails: true)
-      visit edit_inventory_path @inventory
+    it 'users who receive inventory emails redirects to home page' do
+      count
+      sign_in create(:user, send_inventory_emails: true)
+      visit edit_inventory_path inventory
 
-      expect(page).to have_content @inventory.name
+      expect(page).to have_content "You don't have permission"
+      expect(page).to have_content 'Upcoming Builds'
+    end
+
+    it 'data manager shows the page' do
+      count
+      sign_in create(:data_manager)
+      visit edit_inventory_path inventory
+
+      expect(page).to have_content "Edit #{inventory.name}"
+    end
+
+    it 'inventory users shows the page' do
+      count
+      sign_in create(:inventoryist)
+      visit edit_inventory_path inventory
+
+      expect(page).to have_content "Edit #{inventory.name}"
+    end
+
+    it 'admins shows the page' do
+      count
+      sign_in create(:admin)
+
+      visit edit_inventory_path inventory
+
+      expect(page).to have_content "Edit #{inventory.name}"
     end
   end
 
-  context 'when visited by an admin' do
-    before :each do
-      sign_in FactoryBot.create(:admin)
-      visit edit_inventory_path @inventory
-    end
+  context 'has filters' do
+    let(:count) { create :count, inventory: inventory }
 
-    it 'shows the page and filters' do
-      expect(page).to have_content @inventory.name
+    it 'on the page' do
+      count
+      sign_in create(:admin)
+      visit edit_inventory_path inventory
+
+      expect(page).to have_content inventory.name
       expect(page).to have_content 'Filters:'
       expect(page).to have_button 'Show All'
       expect(page).to have_button 'Uncounted'
       expect(page).to have_button 'Partial'
       expect(page).to have_button 'Counted'
       expect(page).to have_css('input#search')
+    end
+  end
 
-      expect(page).to have_content Part.third.name
-      expect(page).to have_content Component.fourth.name
+  context 'when counting items, users can', js: true do
+    before :each do
+      @inventory = create :inventory
+      @user = create :admin
+      sign_in @user
+
+      [['technology 1', 'tech1'], ['technology 2', 'tech2'], ['technology 3', 'tech3']].each do |ary|
+        tech = create :technology, name: ary[0], short_name: ary[1], list_worthy: true
+        create :count_tech, item: tech, inventory: @inventory, loose_count: 0, unopened_boxes_count: 0
+      end
+
+      # create 4 parts w counts
+      create_list :count, 4, inventory: @inventory, loose_count: 0, unopened_boxes_count: 0
+
+      # create 4 components w counts
+      create_list :count_comp, 4, inventory: @inventory, loose_count: 0, unopened_boxes_count: 0
+
+      visit edit_inventory_path @inventory
     end
 
-    it 'allows for filtering by search' do
+    fit 'search' do
+      expect(page).to have_content "Edit #{inventory.name}"
+
       fill_in 'search', with: Part.second.name
 
       expect(page).to have_content Part.second.name
-      expect(page).to have_css("div#count_#{Part.first.id}", visible: false)
-      expect(page).to have_css("div#count_#{Part.third.id}", visible: false)
-      expect(page).to have_css("div#count_#{Component.first.id}", visible: false)
-      expect(page).to have_css("div#count_#{Component.second.id}", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Part.first.uid}]", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Part.third.uid}]", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Component.first.uid}]", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Component.second.uid}]", visible: false)
 
       click_button 'Show All'
+
       fill_in 'search', with: Component.first.name
 
       expect(page).to have_content Component.first.name
-      expect(page).to have_css("div#count_#{Part.first.id}", visible: false)
-      expect(page).to have_css("div#count_#{Part.third.id}", visible: false)
-      expect(page).to have_css("div#count_#{Component.fourth.id}", visible: false)
-      expect(page).to have_css("div#count_#{Component.second.id}", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Part.first.uid}]", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Part.third.uid}]", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Component.fourth.uid}]", visible: false)
+      expect(page).to have_css("div[data-item-uid=#{Component.second.uid}]", visible: false)
     end
 
-    it 'allows for filtering by status' do
+    fit 'filter by status' do
       @count1 = Count.first
-      @count1.user = User.first
-      @count1.save
+      @count1.update(user: @user)
 
-      @count2 = Count.last
-      @count2.user = User.first
-      @count2.save
+      @count2 = Count.second
+      @count2.update(partial_box: true)
 
-      @count3 = Count.second
-      @count3.partial_box = true
-      @count3.save
+      @count3 = Count.third
+      @count3.update(partial_loose: true)
 
-      @count4 = Count.third
-      @count4.partial_loose = true
-      @count4.save
-
+      # refresh the page, wait for it to load
       visit edit_inventory_path @inventory
+      expect(page).to have_content "Edit #{inventory.name}"
 
       click_button 'Uncounted'
 
-      expect(page).to have_content Count.second.name
-      expect(page).to have_content Count.third.name
-      expect(page).to have_css("div#count_#{Count.first.item.id}", visible: false)
-      expect(page).to have_css("div#count_#{Count.last.item.id}", visible: false)
+      expect(page).to have_css("div#count_#{Count.first.id}", visible: false)
+      expect(page).to have_css("div#count_#{Count.second.id}", visible: false)
+      expect(page).to have_css("div#count_#{Count.third.id}", visible: false)
+      expect(page).to have_content Count.fourth.item.name
 
       click_button 'Show All'
       click_button 'Counted'
 
-      expect(page).to have_content Count.first.name
-      expect(page).to have_content Count.last.name
-      expect(page).to have_css("div#count_#{Count.second.item.id}", visible: false)
-      expect(page).to have_css("div#count_#{Count.third.item.id}", visible: false)
+      expect(page).to have_content Count.first.item.name
+      expect(page).to have_css("div#count_#{Count.second.id}", visible: false)
+      expect(page).to have_css("div#count_#{Count.third.id}", visible: false)
 
       click_button 'Show All'
       click_button 'Partial'
 
-      expect(page).to have_content Count.second.name
-      expect(page).to have_content Count.third.name
-      expect(page).to have_css("div#count_#{Count.first.item.id}", visible: false)
-      expect(page).to have_css("div#count_#{Count.last.item.id}", visible: false)
+      expect(page).to have_css("div#count_#{Count.first.id}", visible: false)
+      expect(page).to have_content Count.second.item.name
+      expect(page).to have_content Count.third.item.name
     end
 
-    it 'can be finalized' do
+    # HERE
+    # All submissions should hit CountUpdate.new
+    # Partials: button text ends in 'Count'
+    # Full submissions: button text is 'Edit'
+    pending 'submit a partial box count'
+
+    pending 'submit a partial loose count'
+
+    pending 'submit a full count'
+
+    pending 'finalize the inventory' do
+      allow(CountTransferJob).to receive(:perform_later).with(inventory).and_call_original
+
       click_link 'Finalize'
 
-      expect(InventoriesController::Extrapolate).to receive(:new).with(@inventory).and_call_original
+      expect(CountTransferJob).to receive(:perform_later).with(inventory)
+
       click_button 'Finalize Inventory'
 
-      expect(page).to have_content 'Current Inventory:'
+      expect(page).not_to have_content 'Current Inventory:'
+      expect(page).to have_content 'Inventory Counts:'
     end
   end
 end
