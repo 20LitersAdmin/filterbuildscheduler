00c0b636966259fe215a7aee2c5e324ce1571569
Chip Kragt <chiperific@gmail.com>
2021-11-30 22:02:51 -0500

    try disabling prepared statements elsewhere

--- config/initializers/disable_prepared_statements.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-db_configuration = ActiveRecord::Base.configurations[Rails.env]
-db_configuration.merge!('prepared_statements' => false)
-ActiveRecord::Base.establish_connection(db_configuration)
