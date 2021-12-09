d0b3e9083029d1b2f560d51c23cb0be5a33bf5b0
Chip Kragt <chiperific@gmail.com>
2021-12-09 13:40:11 -0500

    move job triggering from Inventory to InventoriesController#update to prevent infinite job creation"

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -12,8 +12,6 @@ class Inventory < ApplicationRecord
 
   validates :date, presence: true
 
-  after_update :run_count_transfer_job, :run_produceable_job
-
   def count_summary
     summary = "#{item_count} of #{counts.count} items "
     summary +=
@@ -38,6 +36,24 @@ class Inventory < ApplicationRecord
 
   def name
     "#{date.strftime('%-m/%-d/%y')}: #{type}"
+  end
+
+  def run_produceable_job
+    return unless completed_at.present?
+
+    # Delete any jobs that exist, but haven't started, in favor of this new job
+    Delayed::Job.where(queue: 'produceable', locked_at: nil).delete_all
+
+    ProduceableJob.perform_later
+  end
+
+  def run_count_transfer_job
+    return unless completed_at.present?
+
+    # Delete any jobs that exist, but haven't started, in favor of this new job
+    Delayed::Job.where(queue: 'count_transfer', locked_at: nil).delete_all
+
+    CountTransferJob.perform_later(self)
   end
 
   def technologies
@@ -84,23 +100,5 @@ class Inventory < ApplicationRecord
 
   def item_count
     counts.where.not(user_id: nil).count
-  end
-
-  def run_produceable_job
-    return unless completed_at.present?
-
-    # Delete any jobs that exist, but haven't started, in favor of this new job
-    Delayed::Job.where(queue: 'produceable', locked_at: nil).delete_all
-
-    ProduceableJob.perform_later
-  end
-
-  def run_count_transfer_job
-    return unless completed_at.present?
-
-    # Delete any jobs that exist, but haven't started, in favor of this new job
-    Delayed::Job.where(queue: 'count_transfer', locked_at: nil).delete_all
-
-    CountTransferJob.perform_later(self)
   end
 end
