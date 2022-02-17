aeffd49261b3c22127c82ccb2efbf2829a931f7d
Chip Kragt <chiperific@gmail.com>
2022-02-17 13:36:14 -0500

    Pundit::Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
   include ErrorHandler
 
   protect_from_forgery
