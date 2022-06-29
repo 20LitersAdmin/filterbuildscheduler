16ee648ae8c2f3bda8c712faa6e3544beabb17a6
Chip Kragt <chiperific@gmail.com>
2022-06-29 08:15:07 -0700

    feature: InventoriesController::New: user can manually select which technologies to count

--- spec/models/component_spec.rb
+++ spec/models/component_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'concerns/itemable_spec'
+require 'concerns/itemable'
 
 RSpec.describe Component, type: :model do
   it_behaves_like Itemable
