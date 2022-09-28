e579270d96ceb64535f3df0fbe04d3fb1f2f41a7
Chip Kragt <chiperific@gmail.com>
2022-09-28 12:09:18 -0400

    Cron jobs: use sidekiq-scheduler. Sidekiq.yml gets schedule info; scheduler.rake should be unnecessary. Move scheduler.rake logging comments into their various jobs.

--- app/jobs/registration_reminder_job.rb
+++ app/jobs/registration_reminder_job.rb
@@ -1,19 +1,34 @@
 # frozen_string_literal: true
 
+require 'sidekiq-scheduler'
+
 class RegistrationReminderJob < ApplicationJob
   queue_as :registration_reminder
 
   def perform(*_args)
+    puts 'Scheduling Registration Reminders...'
+
     events = Event.pre_reminders.future.within_days(2)
+
+    return 'Done. No events within 2 days.' if events.none?
+
+    e_count = events.size
+    r_count = 0
 
     events.each do |e|
       EventMailer.remind_admins(e).deliver_now
-      e.registrations.pre_reminders.each do |r|
+      registrations = e.registrations.pre_reminders
+
+      r_count += registrations.size
+
+      registrations.each do |r|
         RegistrationMailer.reminder(r).deliver_now
         r.update(reminder_sent_at: Time.zone.now)
       end
 
       e.update(reminder_sent_at: Time.zone.now)
     end
+
+    puts "Done. Sent #{r_count} reminders for #{e_count} events."
   end
 end
--- app/jobs/setup_reminder_job.rb
+++ app/jobs/setup_reminder_job.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
+require 'sidekiq-scheduler'
+
 class SetupReminderJob < ApplicationJob
   queue_as :setup_reminder
 
   def perform(*_args)
+    puts 'Scheduling Setup Reminders...'
+
     setups = Setup.pre_reminders.days_from_now(2)
+
+    return 'Done. No setups within the next 2 days' if setups.none?
+
+    s_count = setups.size
+    r_count = 0
 
     setups.each do |setup|
       next if setup.users.empty?
@@ -12,11 +21,15 @@ class SetupReminderJob < ApplicationJob
       setup.users.each do |user|
         next if setup.reminder_sent_to.include? user.id
 
+        r_count += 1
+
         SetupMailer.remind(setup, user).deliver_now
         setup.reminder_sent_to << user.id
       end
       setup.reminder_sent_at = Time.zone.now
       setup.save
     end
+
+    puts "Done. Sent #{r_count} reminders for #{s_count} setups."
   end
 end
