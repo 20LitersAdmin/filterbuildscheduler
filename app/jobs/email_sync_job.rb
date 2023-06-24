2fb6a4946843e629db8c696895bf4d2e9011378f
Chip <chiperific@gmail.com>
2023-06-24 17:10:33 -0400

    remove temp LoggerNotify stuff

--- app/jobs/email_sync_job.rb
+++ app/jobs/email_sync_job.rb
@@ -21,7 +21,6 @@ class EmailSyncJob < ApplicationJob
     after = Date.yesterday.strftime('%Y/%m/%d')
 
     puts "-+ Syncing emails after:#{after} before:#{before}"
-    log_msg = "-+ Syncing emails after:#{after} before:#{before}"
 
     OauthUser.to_sync.each do |o|
       a_size = Email.all.size
@@ -32,7 +31,6 @@ class EmailSyncJob < ApplicationJob
       # bail if Oauth failure
       if gc.oauth_fail.present?
         puts "-+-+ #{o.name} FAIL: #{gc.oauth_fail}"
-        log_msg += "\n-+-+ #{o.name} FAIL: #{gc.oauth_fail}"
         next
       end
 
@@ -44,17 +42,12 @@ class EmailSyncJob < ApplicationJob
       o.update_column(:last_email_sync, Time.now)
 
       puts "-+-+ Results for #{o.name}: Created #{b_size} emails. Synced #{b_sent} interactions to CRM"
-      log_msg += "\n-+-+ Results for #{o.name}: Created #{b_size} emails. Synced #{b_sent} interactions to CRM"
     end
 
     e_size = Email.stale.size
     Email.stale.destroy_all
     puts "-+ Removed #{e_size} stale emails."
-    log_msg += "\n-+ Removed #{e_size} stale emails."
 
     puts 'Done.'
-
-    # TEMP logging HACK
-    LoggerMailer.notify(OauthUser.first, 'Email Sync Job', "Rough log of events:\n#{log_msg}").deliver_now
   end
 end
