0481bfd6e97778f7984932e70cacaa6a5b761e09
Chip <chiperific@gmail.com>
2026-03-09 10:08:30 -0400

    Switch to mailgun gem from SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,14 +68,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    authentication: :plain,
-    address: 'smtp.mailgun.org',
-    port: 587,
-    domain: '20liters.org',
-    user_name: Rails.application.credentials.dig(:mailgun, :smtp_user),
-    password: Rails.application.credentials.dig(:mailgun, :smtp_password)
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.credentials.dig(:mailgun, :api_key),
+    domain: '20liters.org'
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
